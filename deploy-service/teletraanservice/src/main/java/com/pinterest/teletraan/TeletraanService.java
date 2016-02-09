1c5d81cd2547d88153166218045c31e539703e06
Nick DeChant <nickd@pinterest.com>
2016-02-08 16:22:09 -0800

    Testing Auth with Swagger Ui

--- deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/TeletraanService.java
+++ deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/TeletraanService.java
@@ -41,12 +41,13 @@ public class TeletraanService extends Application<TeletraanServiceConfiguration>
         TeletraanServiceContext context = ConfigHelper.setupContext(configuration);
 
         // Enable CORS headers
-        HashMap<String, String> corsHeaders = new HashMap<String, String>();
-        corsHeaders.put("allowedOrigins", "*");
-        corsHeaders.put("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-        corsHeaders.put("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-        environment.servlets().addFilter("CORS", CrossOriginFilter.class)
-                .setInitParameters(corsHeaders);
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,PUT,POST,DELETE,OPTIONS");
+        filter.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        filter.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowCredentials", "true");
 
         environment.jersey().register(configuration.getAuthenticationFactory().create(context));
 
