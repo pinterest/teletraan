3288baebb6a65c19e6db4a87b2fe2d87dc6ec393
Nick DeChant <nickd@pinterest.com>
2016-02-11 12:08:52 -0800

    Updating demo_run

--- deploy-sentinel/demo_run.sh
+++ deploy-sentinel/demo_run.sh
@@ -8,39 +8,55 @@ RELEASE_VERSION=v1.0.1
 DEPLOY_SERVICE_VERSION=fdd68c0
 DEPLOY_BOARD_VERSION=fdd68c0
 DEPLOY_SENTINEL_VERSION=fdd68c0
-AGENT_VERSION=1.2.2
+AGENT_VERSION=1.2.3
+
+rm -fr ~/teletraan-demo
+rm -fr /tmp/teletraan
+rm -fr /tmp/deployd
+rm -fr /tmp/deploy-board
 
 cd ~
-mkdir teletraan-demo
+mkdir -p teletraan-demo
 
-echo "Install Teletraan server..."
 cd ~/teletraan-demo
-mkdir deploy-service
-wget -O - https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/teletraan-service-${DEPLOY_SERVICE_VERSION}.tar.gz | tar zxf - -C deploy-service
+mkdir -p deploy-service
+curl -L https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/teletraan-service-${DEPLOY_SERVICE_VERSION}.tar.gz | tar zxf - -C deploy-service
+echo "Teletraan server downloaded"
+sed -i '' 's/type: mysql/#type: mysql/' ./deploy-service/bin/server.yaml
+sed -i '' 's/host: localhost/#host: localhost/' ./deploy-service/bin/server.yaml
+sed -i '' 's/port: 3306/#port: 3306/' ./deploy-service/bin/server.yaml
+sed -i '' 's/userName: root/#userName: root/' ./deploy-service/bin/server.yaml
+sed -i '' 's/password:/#password:/' ./deploy-service/bin/server.yaml
+sed -i '' 's/pool: 10:50:20:5/#pool: 10:50:20:5/' ./deploy-service/bin/server.yaml
+sed -i '' 's/#type: embedded/type: embedded/' ./deploy-service/bin/server.yaml
+sed -i '' 's/#workDir: \/tmp\/teletraan\/db/workDir: \/tmp\/teletraan\/db/' ./deploy-service/bin/server.yaml
+echo "Edited server.yaml to user embedded database"
 ./deploy-service/bin/run.sh start
-echo "Successfully installed Teletraan Server"
+echo "Teletraan server is running"
 
-echo "Install Deploy Board..."
 cd ~/teletraan-demo
 virtualenv ./venv
 source ./venv/bin/activate
-mkdir deploy-board
-wget -O - https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-board-${DEPLOY_BOARD_VERSION}.tar.gz | tar zxf - -C deploy-board --strip-components=1
+mkdir -p deploy-board
+curl -L https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-board-${DEPLOY_BOARD_VERSION}.tar.gz | tar zxf - -C deploy-board --strip-components=1
+echo "Deploy board downloaded"
 cd deploy-board
 pip install -r requirements.txt
+mkdir -p /tmp/deploy_board
 ./run.sh start
-echo "Successfully installed Deploy Board"
+echo "Deploy board is running"
 
-echo "Install Deploy Sentinel..."
 cd ~/teletraan-demo
-mkdir deploy-sentinel
-wget -O - https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-sentinel-${DEPLOY_SENTINEL_VERSION}.tar.gz | tar zxf - -C deploy-sentinel --strip-components=1
-echo "Successfully installed deploy-sentinel"
+mkdir -p deploy-sentinel
+curl -L https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-sentinel-${DEPLOY_SENTINEL_VERSION}.tar.gz | tar zxf - -C deploy-sentinel --strip-components=1
+cd ~/teletraan-demo/deploy-sentinel
+echo "Deploy sentinel downloaded"
 
-echo "Install Deploy Agent..."
 cd ~/teletraan-demo
-wget https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-agent-${AGENT_VERSION}.zip
-pip install deploy-agent==${AGENT_VERSION} --find-links=file:///home/vagrant/teletraan-demo/deploy-agent-${AGENT_VERSION}.zip
-echo "Successfully installed Deploy Agent"
+mkdir -p deploy-agent
+curl -L https://github.com/pinterest/teletraan/releases/download/${RELEASE_VERSION}/deploy-agent-${AGENT_VERSION}.zip > ./deploy-agent/deploy-agent-${AGENT_VERSION}.zip
+echo "Deploy agent downloaded"
+CURRENT_PATH=${PWD}
+pip install deploy-agent==${AGENT_VERSION} --find-links=file://${CURRENT_PATH}/deploy-agent/deploy-agent-${AGENT_VERSION}.zip
 
 echo "Successfully completed Teletraan installation. Access http://localhost:8888 to try it out!"
