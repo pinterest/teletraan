2d7e24244f8b9facea10a12e327abfe686086f7c
Ming Hao <haom@pinterest.com>
2016-06-24 18:53:53 +0000

    fix bugs exposed by one prod box testing.

--- deploy-agent/deployd/client/serverless_client.py
+++ deploy-agent/deployd/client/serverless_client.py
@@ -28,7 +28,7 @@ from deployd.types.ping_response import PingResponse
 
 log = logging.getLogger(__name__)
 
-_DEPLOY_STAGE_TRANSITIONS = dict(map(lambda i: (i, i+1), range(DeployStage.PRE_DOWNLOAD, DeployStage.POST_RESTART)))
+_DEPLOY_STAGE_TRANSITIONS = dict(map(lambda i: (i, i+1), range(DeployStage.PRE_DOWNLOAD, DeployStage.SERVING_BUILD)))
 
 
 class ServerlessClient(object):
@@ -40,13 +40,14 @@ class ServerlessClient(object):
       PRE_DOWNLOAD->DOWNLOADING->POST_DOWNLOAD->STAGING->PRE_RESTART
           ->RESTARTING->POST_RESTART->SERVING_BUILD
     """
-    def __init__(self, env_name, stage, build):
+    def __init__(self, env_name, stage, build, script_variables):
         """build contains build information in json format. It contains information defined in types/build.py.
         """
         self._env_name = utils.check_not_none(env_name, 'env_name can not be None')
         self._stage = utils.check_not_none(stage, 'stage name can not be None')
         self._build = json.loads(utils.check_not_none(build, 'build can not be None'))
-        self._deploy_id = uuid.uuid4()
+        self._script_variables = json.loads(utils.check_not_none(script_variables, 'script_variables can not be None'))
+        self._deploy_id = uuid.uuid4().hex
               
     def send_reports(self, env_reports=None):
         reports = [status.report for status in env_reports.values()]
@@ -55,44 +56,48 @@ class ServerlessClient(object):
                 continue
             self._env_id = report.envId 
             ping_response = self._create_response(report)
-            log.debug('%s -> %s' % (reports, ping_response))
+            log.info('%s -> %s' % (reports, ping_response))
 
             return ping_response
-        return None
+
+        # env_status file might be corrupted or first deploy.
+        self._env_id = uuid.uuid4().hex
+        return self._create_response(None)
 
     def _create_response(self, report):
         # check if this is the first step
-        if report.deployId is None or report.deployId != self._deploy_id:
+        if report is None or report.deployId is None or report.deployId != self._deploy_id:
             # first report from agent, start first deploy stage.
-            return _new_response_value(DeployStage.PRE_DOWNLOAD)
+            return self._new_response_value(DeployStage.PRE_DOWNLOAD)
         if report.errorCode != 0:
             # terminate the deployment.
             return None
-        numeric_deploy_stage = DeployStage._NAMES_TO_VALUES(report.get('deployStage')) 
+        numeric_deploy_stage = DeployStage._NAMES_TO_VALUES[report.deployStage] 
         if report.status == AgentStatus.SUCCEEDED:
             # check if this is the last deploy stage.
             if numeric_deploy_stage == DeployStage.SERVING_BUILD:
-                return {'opCode': OperationCode.NOOP}
+                return PingResponse({'opCode': OperationCode.NOOP})
 
             # move to next deploy stage
             next_deploy_stage = _DEPLOY_STAGE_TRANSITIONS.get(numeric_deploy_stage)
-            return _new_response_value(next_deploy_stage)
+            return self._new_response_value(next_deploy_stage)
 
         if report.status == AgentStatus.UNKNOWN:
             # retry current stage
-            return _new_response_value(numeric_deploy_stage)
+            return self._new_response_value(numeric_deploy_stage)
 
         # terminate deployment
         return None
 
-    def _new_response_value(numeric_deploy_stage):
-        value = {'opCode': OperationCode.DEPLOY,
-                 'deployGoal': {'deployId': self._deploy_id,
-                                'envId': self._env_id,
-                                'envName': self._env_name,
-                                'stageName': self._stage,
-                                'deployStage': numeric_deploy_stage}}
+    def _new_response_value(self, numeric_deploy_stage):
+        value= {'opCode': OperationCode.DEPLOY,
+                'deployGoal': {'deployId': self._deploy_id,
+                               'envId': self._env_id,
+                               'envName': self._env_name,
+                               'stageName': self._stage,
+                               'deployStage': numeric_deploy_stage}}
         if numeric_deploy_stage == DeployStage.DOWNLOADING:
-            value['build'] = self._build
-        return value
+            value['deployGoal']['build'] = self._build
+            value['deployGoal']['scriptVariables'] = self._script_variables
+        return PingResponse(value)
  
