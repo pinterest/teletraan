0f6628f9b7253d3faa631cb1cf532a5fb65247ee
Suli Xu <suli@pinterest.com>
2017-04-25 16:40:19 -0700

    varchar(32) to 2048

---
+++ tools/mysql/schema-update-2.sql
@@ -0,0 +1,11 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# This script upgrade DB schema from version 1 to version 2
+
+ALTER TABLE schedules MODIFY COLUMN cooldown_times VARCHAR(2048);
+ALTER TABLE schedules MODIFY COLUMN host_numbers VARCHAR(2048);
+
+-- make sure to update the schema version to current+1
+UPDATE schema_versions SET version=2;
