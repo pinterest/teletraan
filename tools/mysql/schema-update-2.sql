e4ec5572eb3df4c86c400023a319473f90539b72
Suli Xu <suli@pinterest.com>
2017-04-26 10:01:33 -0700

    ADD NOT NULL

--- tools/mysql/schema-update-2.sql
+++ tools/mysql/schema-update-2.sql
@@ -4,8 +4,8 @@
 
 # This script upgrade DB schema from version 1 to version 2
 
-ALTER TABLE schedules MODIFY COLUMN cooldown_times VARCHAR(2048);
-ALTER TABLE schedules MODIFY COLUMN host_numbers VARCHAR(2048);
+ALTER TABLE schedules MODIFY COLUMN cooldown_times VARCHAR(2048) NOT NULL;
+ALTER TABLE schedules MODIFY COLUMN host_numbers VARCHAR(2048) NOT NULL;
 
 -- make sure to update the schema version to current+1
 UPDATE schema_versions SET version=2;
