c6a45bf0a7decdb4d49fc17bfa540762e05b57e1
Lida Li <lidali@pinterest.com>
2017-12-13 11:27:20 -0800

    Adjust the number and format

--- deploy-service/common/src/main/java/com/pinterest/deployservice/handler/DeployHandler.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/handler/DeployHandler.java
@@ -397,6 +397,7 @@ public class DeployHandler implements DeployHandlerInterface{
         int maxPages = 50; //This makes us check at most 5000 deploys
         int tocheckPages = maxPages;
         while (tocheckPages-- > 0) {
+        while (maxPages-- > 0) {
             DeployQueryFilter filter = new DeployQueryFilter(filterBean);
             DeployQueryResultBean resultBean = deployDAO.getAllDeploys(filter);
             if (resultBean.getTotal() < 1) {
