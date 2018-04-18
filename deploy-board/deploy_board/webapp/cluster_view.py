efac74f1ca49afbc8395dbf2c0639964a923b5a4
Lida Li <lilida@lilida.org>
2018-04-18 09:49:51 -0700

    Add root_volume_size option (#580)

--- deploy-board/deploy_board/webapp/cluster_view.py
+++ deploy-board/deploy_board/webapp/cluster_view.py
@@ -700,6 +700,7 @@ def get_aws_config_name_list_by_image(image_name):
         config_map['ebs_size'] = 500
         config_map['ebs_mount'] = '/backup'
         config_map['ebs_volume_type'] = 'gp2'
+        config_map['root_volume_size'] = 100
         if image_name == DEFAULT_CMP_IMAGE:
             config_map['pinfo_role'] = 'cmp_base'
             config_map['pinfo_team'] = 'cloudeng'
