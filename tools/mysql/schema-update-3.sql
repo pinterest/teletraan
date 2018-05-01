f49a303eae88029ede56b8f2c7b19ae980f3c767
Suli Xu <suli@pinterest.com>
2018-05-01 14:36:15 -0700

    address the comments

--- tools/mysql/schema-update-3.sql
+++ tools/mysql/schema-update-3.sql
@@ -5,7 +5,7 @@
 # This script upgrade DB schema from version 2 to version 3
 
 
-ALTER TABLE environs ADD COLUMN project_id VARCHAR(32);
+ALTER TABLE environs ADD COLUMN project_id INT(11);
 
 -- make sure to update the schema version to 3
 UPDATE schema_versions SET version=3;
