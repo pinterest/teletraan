1aa36b0da794c57d33a085afd393e1804102d19d
Suli Xu <suli@pinterest.com>
2018-05-09 14:32:34 -0700

    need to drop project_id column

--- tools/mysql/schema-update-4.sql
+++ tools/mysql/schema-update-4.sql
@@ -4,7 +4,7 @@
 
 # This script upgrade DB schema from version 3 to version 4
 
-
+ALTER TABLE environs DROP COLUMN project_id;
 ALTER TABLE environs ADD COLUMN external_id CHAR(36);
 
 -- make sure to update the schema version to 4
