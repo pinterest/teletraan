23380b352b8c343873d81c33aa827aff584cbcf1
Lat Ware <lat@pinterest.com>
2018-10-29 14:23:17 -0700

    Fixing api-docs.

--- deploy-board/deploy_board/settings.py
+++ deploy-board/deploy_board/settings.py
@@ -289,7 +289,8 @@ if IS_PINTEREST:
     DEFAULT_CMP_ACCESS_ROLE = os.getenv('DEFAULT_CMP_ACCESS_ROLE')
 
     #CSP Config
-    CSP_SCRIPT_SRC = ("'self'", "https://www.google.com/ 'unsafe-inline' 'unsafe-eval'")
+    CSP_SCRIPT_SRC = ("'self'", "https://www.google.com/ 'unsafe-inline' 'unsafe-eval'",
+                      "https://teletraan.pinadmin.com/swagger.json")
     CSP_DEFAULT_SRC = ("'self'")
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 
