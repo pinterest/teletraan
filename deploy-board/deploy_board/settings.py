f4136dcba57091941160c59eef376fc38c5782ff
TheLat <thouartaninny@gmail.com>
2018-10-30 08:59:54 -0700

    Fixing api-docs (#618)
    * Fixing api-docs.
    
    * Fixing api-docs
    
    * Fixing api-docs in a proper way.

--- deploy-board/deploy_board/settings.py
+++ deploy-board/deploy_board/settings.py
@@ -291,5 +291,7 @@ if IS_PINTEREST:
     #CSP Config
     CSP_SCRIPT_SRC = ("'self'", "https://www.google.com/ 'unsafe-inline' 'unsafe-eval'")
     CSP_DEFAULT_SRC = ("'self'")
+    CSP_CONNECT_SRC = ("'self'")
+    CSP_EXCLUDE_URL_PREFIXES = ('/api-docs',)
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
 
