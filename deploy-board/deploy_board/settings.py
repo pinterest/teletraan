e22d052cf7e34b8084beb00a5059a282f7216e23
Yongwen Xu <yongwen@users.noreply.github.com>
2019-06-07 09:41:33 -0700

    fix the logger for manage_local (#679)

--- deploy-board/deploy_board/settings.py
+++ deploy-board/deploy_board/settings.py
@@ -104,7 +104,7 @@ else:
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'formatters': {
         'standard': {
             'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
