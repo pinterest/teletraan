2cd2f360c85d5212d6ba2d5bac8cc1f180551bb9
lixmgl <lixmgl@gmail.com>
2019-11-10 09:52:25 -0800

    add a new flag in build db to protect deploy only happens from trusted artifact build
    move new flag into EnvBean instead of BuildBean
    
    correct buildBean and EnvBean settings
    
    update test
    
    update warning message to be more clear

---
+++ tools/mysql/schema-update-6.sql
@@ -0,0 +1,10 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# This script upgrade DB schema from version 5 to version 6
+
+ALTER TABLE environs ADD COLUMN ensure_trusted_build TINYINT(1) NOT NULL DEFAULT 0;
+
+-- make sure to update the schema version to 5
+UPDATE schema_versions SET version=6;
