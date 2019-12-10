51d2a7a35bd1e2376bde4b978a0e974811134cae
lixmgl <lixmgl@gmail.com>
2019-12-09 23:34:53 -0800

    set default image type as 18.04

--- deploy-board/deploy_board/settings.py
+++ deploy-board/deploy_board/settings.py
@@ -286,7 +286,7 @@ if IS_PINTEREST:
     DEFAULT_PROVIDER = 'AWS'
 
     #Pinterest Default AMI image name
-    DEFAULT_CMP_IMAGE = 'cmp_base-ebs'
+    DEFAULT_CMP_IMAGE = 'cmp_base-ebs-18.04'
 
     #Pinterest Default Host Type
     DEFAULT_CMP_HOST_TYPE = 'EbsComputeLo(Recommended)'
