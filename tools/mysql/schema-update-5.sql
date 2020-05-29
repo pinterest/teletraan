02fbd63cd67efdedd309f1ade07786ea19277e11
Ankit Shah <ashah@pinterest.com>
2020-05-29 16:53:58 -0700

    Chatroom size (#716)
    * increase chatroom size
    
    * newline

--- tools/mysql/schema-update-5.sql
+++ tools/mysql/schema-update-5.sql
@@ -6,5 +6,5 @@
 
 ALTER TABLE environs ADD COLUMN allow_private_build TINYINT(1) NOT NULL DEFAULT 0;
 
--- make sure to update the schema version to 4
+-- make sure to update the schema version to 5
 UPDATE schema_versions SET version=5;
--- tools/mysql/schema-update-6.sql
+++ tools/mysql/schema-update-6.sql
@@ -6,5 +6,5 @@
 
 ALTER TABLE environs ADD COLUMN ensure_trusted_build TINYINT(1) NOT NULL DEFAULT 0;
 
--- make sure to update the schema version to 5
+-- make sure to update the schema version to 6
 UPDATE schema_versions SET version=6;
