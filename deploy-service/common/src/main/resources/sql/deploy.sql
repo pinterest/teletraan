e01062ad7d8261ba56126e57bc3e4036ff053e0b
Yongwen Xu <yongwen@pinterest.com>
2020-09-15 23:46:04 -0700

    fix the sql change

--- deploy-service/common/src/main/resources/sql/deploy.sql
+++ deploy-service/common/src/main/resources/sql/deploy.sql
@@ -42,6 +42,7 @@ CREATE TABLE IF NOT EXISTS environs (
     external_id          CHAR(36),
     allow_private_build TINYINT(1)    NOT NULL DEFAULT 0,
     ensure_trusted_build TINYINT(1)    NOT NULL DEFAULT 0,
+    stage_type VARCHAR(32) NOT NULL DEFAULT 'PRODUCTION',
     PRIMARY KEY   (env_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE UNIQUE INDEX env_name_stage_idx ON environs (env_name, stage_name);
