65615615ab3bc03ee7372bb3695c32ca859c50e7
Ankit Shah <ashah@pinterest.com>
2020-09-19 19:42:51 -0700

    Index cluster name in environs table (#766)
    * validate from stage name
    
    * index cluster name in environs table

---
+++ tools/mysql/schema-update-10.sql
@@ -0,0 +1,10 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# This script upgrade DB schema from version 9 to version 10
+
+CREATE INDEX cluster_name ON environs (cluster_name);
+
+-- make sure to update the schema version to 10
+UPDATE schema_versions SET version=10;
