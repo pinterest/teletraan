916b467ca645f56b803f94fe91d08bcfb4ffacf2
Ankit Shah <ashah@pinterest.com>
2020-12-09 11:46:17 -0800

    Agent count table cache implementation (#788)
    * validate from stage name
    
    * chatroom is a list of channels separated by comma and sometimes space
    
    * don't block the deployment of lower priority env if higher priority env was previously deployed on the host
    
    * Revert "don't block the deployment of lower priority env if higher priority env was previously deployed on the host"
    
    This reverts commit 56d6ad70b97e5473e8bdd737de953c65072ecc46.
    
    * don't block the deployment of lower priority env if higher priority env was previously deployed on the host (#786)
    
    * agent count table impl.
    
    * count cache impl.
    
    * revert proxysql change
    
    * cleanup
    
    * add comments and remove dead code
    
    * resolve conflict
    
    * f
    
    * f

---
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/bean/AgentCountBean.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2020 Pinterest, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pinterest.deployservice.bean;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+
+/**
+ * Keep the bean and table in sync
+ * <p/>
+ * CREATE TABLE agent_count (
+ * env_id   VARCHAR(22)    NOT NULL,
+ * deploy_id VARCHAR(22)    NOT NULL,
+ * existingCount    INT            NOT NULL DEFAULT 0,
+ * activeCount    INT            NOT NULL DEFAULT 0,
+ * last_refresh    BIGINT              NOT NULL,
+ * PRIMARY KEY (env_id)
+ * );
+ */
+public class AgentCountBean implements Updatable {
+    @JsonProperty("envId")
+    private String env_id;
+
+    @JsonProperty("deployId")
+    private String deploy_id;
+
+    @JsonProperty("existingCount")
+    private Long existing_count;
+
+    @JsonProperty("activeCount")
+    private Long active_count;
+
+    @JsonProperty("lastRefresh")
+    private Long last_refresh;
+
+    public String getEnv_id() {
+        return env_id;
+    }
+
+    public void setEnv_id(String env_id) {
+        this.env_id = env_id;
+    }
+
+    public String getDeploy_id() {
+        return deploy_id;
+    }
+
+    public void setDeploy_id(String deploy_id) {
+        this.deploy_id = deploy_id;
+    }
+
+    public Long getExisting_count() {
+        return existing_count;
+    }
+
+    public void setExisting_count(Long existing_count) {
+        this.existing_count = existing_count;
+    }
+
+    public void setActive_count(Long active_count) {
+        this.active_count = active_count;
+    }
+
+    public Long getActive_count() {
+        return active_count;
+    }
+
+    public Long getLast_refresh() {
+        return last_refresh;
+    }
+
+    public void setLast_refresh(Long last_refresh) {
+        this.last_refresh = last_refresh;
+    }
+
+    @Override
+    public SetClause genSetClause() {
+        SetClause clause = new SetClause();
+        clause.addColumn("env_id", env_id);
+        clause.addColumn("deploy_id", deploy_id);
+        clause.addColumn("existing_count", existing_count);
+        clause.addColumn("active_count", active_count);
+        clause.addColumn("last_refresh", last_refresh);
+        return clause;
+    }
+
+    public final static String UPDATE_CLAUSE =
+        "env_id=VALUES(env_id)," +
+        "deploy_id=VALUES(deploy_id)," +
+        "existing_count=VALUES(existing_count)," +
+        "active_count=VALUES(active_count)," +
+        "last_refresh=CASE WHEN last_refresh IS NOT NULL THEN VALUES(last_refresh) ELSE last_refresh END";
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this);
+    }
+}
---
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/db/DBAgentCountDAOImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2020 Pinterest, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pinterest.deployservice.db;
+
+import com.pinterest.deployservice.bean.AgentCountBean;
+import com.pinterest.deployservice.bean.SetClause;
+import com.pinterest.deployservice.dao.AgentCountDAO;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+/* count table cache to store # of actively deploying agents and # of existing agents */
+public class DBAgentCountDAOImpl implements AgentCountDAO {
+    private static final String GET_COUNT =
+        "SELECT * FROM agent_counts WHERE env_id=?";
+    private static final String DELETE_COUNT =
+        "DELETE * FROM agent_counts WHERE env_id=?";
+    private static final String INSERT_OR_UPDATE_COUNT =
+        "INSERT INTO agent_counts SET %s ON DUPLICATE KEY UPDATE %s";
+
+    private BasicDataSource dataSource;
+
+    public DBAgentCountDAOImpl(BasicDataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public AgentCountBean get(String envId) throws Exception {
+        ResultSetHandler<AgentCountBean> h = new BeanHandler<AgentCountBean>(AgentCountBean.class);
+        return new QueryRunner(dataSource).query(GET_COUNT, h, envId);
+    }
+
+    @Override
+    public void delete(String envId) throws Exception {
+        ResultSetHandler<AgentCountBean> h = new BeanHandler<AgentCountBean>(AgentCountBean.class);
+        new QueryRunner(dataSource).query(DELETE_COUNT, h, envId);
+    }
+
+    @Override
+    public void insertOrUpdate(AgentCountBean agentCountBean) throws Exception {
+        SetClause setClause = agentCountBean.genSetClause();
+        String clause = String.format(INSERT_OR_UPDATE_COUNT, setClause.getClause(), AgentCountBean.UPDATE_CLAUSE);
+        new QueryRunner(dataSource).update(clause, setClause.getValueArray());
+    }
+}
