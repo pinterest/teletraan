f77dc42a43903568b4dbf9ff87f815965e197b7b
fsilvadefaria <fsilvadefaria@pinterest.com>
2021-03-18 17:22:17 -0300

    Bugfix on schema-update scripts 11, 7, 9, 8
    Fixed encoding for files saved as UTF8 to ASCII
    
    Using space instead of No-Break-Space

--- tools/mysql/schema-update-9.sql
+++ tools/mysql/schema-update-9.sql
@@ -4,7 +4,7 @@
 
 # This script upgrade DB schema from version 8 to version 9
 
-ALTER TABLE hosts MODIFY group_name VARCHAR(128) NOT NULL;
+ALTER TABLE hosts MODIFY group_name VARCHAR(128) NOT NULL;
 
 -- make sure to update the schema version to 9
 UPDATE schema_versions SET version=9;
