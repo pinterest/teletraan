83bf72af2d940547fd455f11a5d171a0dccc14e0
Trent Robbins <trobbins@pinterest.com>
2021-05-11 19:00:56 -0700

    Change schema and bump version, which is a few years behind. Doesn't seem to be used.

---
+++ tools/mysql/schema-update-13.sql
@@ -0,0 +1,10 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# This script upgrade DB schema from version 4 to version 5
+
+ALTER TABLE environs ADD COLUMN is_sox TINYINT(1) NOT NULL DEFAULT 0;
+
+-- make sure to update the schema version to 5
+UPDATE schema_versions SET version=13;
