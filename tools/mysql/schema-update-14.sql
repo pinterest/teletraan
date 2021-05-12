edee5b59e685a23ec64c05aafbfc9a6c9113ad4a
Trent Robbins <trobbins@pinterest.com>
2021-05-12 11:27:43 -0700

    Draft: remove outdated instruction and undo change to deploy.sql.

--- tools/mysql/schema-update-14.sql
+++ tools/mysql/schema-update-14.sql
@@ -2,9 +2,9 @@
 -- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
 -- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-# This script upgrade DB schema from version 4 to version 5
+# This script upgrade DB schema from version 13 to version 14
 
 ALTER TABLE environs ADD COLUMN is_sox TINYINT(1) NOT NULL DEFAULT 0;
 
 -- make sure to update the schema version to 5
-UPDATE schema_versions SET version=13;
+UPDATE schema_versions SET version=14;
