bb7af938f5fa5504df1026b51fc792592a11b222
Trent Robbins <trobbins@pinterest.com>
2021-08-16 21:29:39 -0700

    Add unique parameter to sc_config_id. DANGER: Applying this will delete duplicate values after the first.

---
+++ tools/mysql/schema-update-15.sql
@@ -0,0 +1,9 @@
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+# This script upgrade DB schema from version 14 to version 15
+
+ALTER TABLE environs ADD UNIQUE (sc_config_id);
+
+UPDATE schema_versions SET version=15;
