b32614215fcda04f39d0d6c9837b8e0d3ca52264
Trent Robbins <trobbins@pinterest.com>
2021-11-01 18:52:02 +0000

    Clean up scripts and configs.

--- deploy-agent/unit_tests_docker.sh
+++ deploy-agent/unit_tests_docker.sh
@@ -3,6 +3,5 @@
 set -ex
 
 BUILD_NAME=deployd_debug
-#docker rmi "$BUILD_NAME"  # don't always run; fails anyways if containers are running, force it -f
 docker build -t "$BUILD_NAME" .
 docker run -it "$BUILD_NAME" bash
