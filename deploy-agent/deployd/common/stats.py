8aefad0915e0270fc4fe791686af3b7a94ca02d9
kynanlalone <kynanlalone@gmail.com>
2021-11-10 19:37:11 -0700

    add gauge

--- deploy-agent/deployd/common/stats.py
+++ deploy-agent/deployd/common/stats.py
@@ -3,9 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#  
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#    
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,5 +35,13 @@ def create_sc_increment(stats, sample_rate, tags):
     if IS_PINTEREST:
         from pinstatsd.statsd import sc
         sc.increment(stats, sample_rate, tags)
+    else:
+        return
+
+
+def create_sc_gauge(stats, value, sample_rate, tags):
+    if IS_PINTEREST:
+        from pinstatsd.statsd import sc
+        sc.gauge(stats, value, sample_rate=sample_rate, tags=tags)
     else:
         return
