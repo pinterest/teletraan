8d5504a3caefb5f5fe3d340af26a62d286996d8b
kynanlalone <kynanlalone@gmail.com>
2022-01-28 09:31:45 -0700

    add tests for metric caching

--- deploy-agent/tests/unit/deploy/common/test_stats.py
+++ deploy-agent/tests/unit/deploy/common/test_stats.py
@@ -12,12 +12,146 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tests
+from deployd.common.stats import Cache, Stat, MetricClient, TimeElapsed
+from deployd import __version__
+import unittest
+import os
+import json
+import socket
 from time import sleep
-from deployd.common.stats import TimeElapsed
+try:
+    from unittest import mock
+except ImportError:
+    # python2 support
+    import mock
 
 
-class TestTimeElapsed(tests.TestCase):
+class TestCache(unittest.TestCase):
+    path = 'tests/unit/deploy/common/test_stats.cache'
+    data = 'testdata'
+
+    def test_exists(self):
+        cache = Cache(self.path)
+        self.assertTrue(cache.exists())
+
+    def test_is_empty(self):
+        cache = Cache(self.path)
+        cache.truncate()
+        self.assertTrue(cache.is_empty())
+
+    def test_read(self):
+        cache = Cache(self.path)
+        cache.truncate()
+        with open(self.path, 'w') as fh:
+            fh.write('{}\n'.format(self.data))
+        count = 0
+        for _ in cache.read():
+            count += 1
+        self.assertEqual(count, 1)
+
+    def test_write(self):
+        cache = Cache(self.path)
+        cache.truncate()
+        cache.write(self.data)
+        with open(self.path, 'r') as fh:
+            validate = fh.read()
+        expect = '{}\n'.format(self.data)
+        self.assertEqual(validate, expect)
+
+    def tearDown(self):
+        Cache(self.path)
+        os.remove(self.path)
+
+
+class TestStat(unittest.TestCase):
+    mtype = 'increment'
+    name = 'name'
+    value = 1
+    sample_rate = 1.0
+    tags = {'key': 'value'}
+    data = ('{{"mtype": "{0}", '
+            '"name": "{1}", '
+            '"value": {2}, '
+            '"sample_rate": {3}, '
+            '"tags": {4}}}')
+
+    def test_serialize(self):
+        stat = Stat(mtype=self.mtype,
+                    name=self.name,
+                    value=self.value,
+                    sample_rate=self.sample_rate,
+                    tags=self.tags)
+        self.assertIsInstance(stat.serialize(), str)
+
+    def test__deserialize(self):
+        data = self.data.format(self.mtype,
+                                self.name,
+                                self.value,
+                                self.sample_rate,
+                                json.dumps(self.tags))
+        stat = Stat(mtype=None,
+                    name=None,
+                    value=None,
+                    sample_rate=None,
+                    tags=None,
+                    ins=data)
+        stat._deserialize()
+        self.assertEqual(stat.mtype, self.mtype)
+        self.assertEqual(stat.name, self.name)
+        self.assertEqual(stat.sample_rate, self.sample_rate)
+        self.assertEqual(stat.tags, self.tags)
+
+
+    def test_deserialize(self):
+        stat = Stat(mtype=None,
+                    name=None,
+                    value=None,
+                    sample_rate=None,
+                    tags=None)
+        data = self.data.format(self.mtype,
+                                self.name,
+                                self.value,
+                                self.sample_rate,
+                                json.dumps(self.tags))
+        self.assertTrue(stat.deserialize(ins=data))
+        invalid_str = '{1:'
+        self.assertFalse(stat.deserialize(ins=invalid_str))
+        invalid_type = []
+        self.assertFalse(stat.deserialize(ins=invalid_type))
+
+
+class TestMetricClient(unittest.TestCase):
+    cache_path = 'tests/unit/deploy/common/test_stats.cache'
+    port = 5000
+
+    def setUp(self):
+        self.client = MetricClient(port=self.port, cache_path=self.cache_path)
+
+    def test__add_default_tags(self):
+        tag_version = {'deploy_agent_version': __version__}
+        tags = self.client._add_default_tags()
+        if not __version__:
+            self.assertEqual(tags, None)
+        else:
+            self.assertEqual(tags, tag_version)
+        tag_test = {'test': 'data'}
+        tags = self.client._add_default_tags(tag_test)
+        self.assertEqual(tags, dict(tag_version, **tag_test))
+
+    @mock.patch('socket.socket.connect_ex')
+    def test_is_healthy(self, mock_connect_ex):
+        mock_connect_ex.return_value = 0
+        self.assertTrue(self.client.is_healthy())
+        mock_connect_ex.return_value = 50
+        self.assertFalse(self.client.is_healthy())
+        mock_connect_ex.raiseError.side_effect = Exception(socket.error)
+        self.assertFalse(self.client.is_healthy())
+
+    def tearDown(self):
+        Cache(self.cache_path)
+        os.remove(self.cache_path)
+
+class TestTimeElapsed(unittest.TestCase):
 
     def test__is_paused(self):
         elapsed = TimeElapsed()
