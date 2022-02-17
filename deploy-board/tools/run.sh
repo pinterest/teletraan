739de3ea4ec042d1ef49b58721bb6b45db090eab
YaqinLi <yaqinli@pinterest.com>
2022-02-17 01:40:24 +0000

    use correct dependency version

--- deploy-board/tools/run.sh
+++ deploy-board/tools/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export TELETRAAN_SERVICE_URL=http://localhost:8011
+export TELETRAAN_SERVICE_URL=http://localhost:8080
 export TELETRAAN_SERVICE_VERSION=v1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET=$DIR/$1
--- deploy-service/teletraanservice/bin/run.sh
+++ deploy-service/teletraanservice/bin/run.sh
@@ -48,8 +48,6 @@ function server_start {
     com.pinterest.teletraan.TeletraanService \
     server ${CONFIG_FILE}"
 
-    echo $OPTS
-
     if [ "$1" == "FOREGROUND" ]
     then
         ${JAVA_CMD} ${OPTS}
