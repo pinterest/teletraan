7cea2b5ff03688ce3c5e1014f2fc1f6081541a93
Ankit Shah <ashah@pinterest.com>
2022-03-18 17:52:35 -0700

    f

--- deploy-agent/deployd/client/client.py
+++ deploy-agent/deployd/client/client.py
@@ -44,6 +44,7 @@ class Client(BaseClient):
         self._autoscaling_group = None
         self._availability_zone = None
         self._stage_type = None
+        self._host_info_populated = False
         try:
             if self._read_host_info() is False:
                 log.error("Fail to read host info")
@@ -57,6 +58,9 @@ class Client(BaseClient):
                                 tags={'host': self._hostname}) 
 
     def _read_host_info(self):
+        if self._host_info_populated:
+            log.debug("host info already populated")
+            return True
         if self._use_facter:
             log.info("Use facter to get host info")
             name_key = self._config.get_facter_name_key()
@@ -183,6 +187,7 @@ class Client(BaseClient):
                  "Host name: {}, IP: {}, host id: {}, agent_version={}, autoscaling_group: {}, "
                  "availability_zone: {}, stage_type: {}, group: {}".format(self._hostname, self._ip, self._id, 
                  self._agent_version, self._autoscaling_group, self._availability_zone, self._stage_type, self._hostgroup))
+        self._host_info_populated = True
         return True
 
     def send_reports(self, env_reports=None):
