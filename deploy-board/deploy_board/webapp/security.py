b299ee24ce7ee94518f90f27a7f18652db543da3
Tyler Ouyang <touyang@pinterest.com>
2022-07-25 10:49:38 -0700

    log more messages

--- deploy-board/deploy_board/webapp/security.py
+++ deploy-board/deploy_board/webapp/security.py
@@ -69,7 +69,7 @@ class DelegatedOAuthMiddleware(object):
         else:
             # TODO call logout to remove session cleanly
             # self.logout(request)
-            data = {'origin_path': request.get_full_path()}
+            data = {'origin_path': request.get_full_path(), 'headers': request.headers, 'meta': request.META}
             url = self.oauth.get_authorization_url(session=request.session, data=data)
             logger.debug("Redirect oauth for authentication!, url = " + url)
             return HttpResponseRedirect(url)
