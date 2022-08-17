pinterest/teletraan

commit d468d1c695a603850cacd290d130614ad9083b48

Added inline form warning when users try to over-resize ASGs (#987)
Added inline form warning when users try to over-resize ASGs. Specifically,

Unified env view and group view min/max size UI components by extending asg-capacity-config.
Updated asg-capacity-config and static-capacity-config to support displaying inline form warning and error.
Extended EnvCapacityConfigView.get() and get_asg_config() to return placements so the frontend can check remaining capacity.

deploy-board/deploy_board/webapp/capacity_views.py
@@ -20,18 +20,18 @@ import logging
 from django.http import HttpResponse
 from django.middleware.csrf import get_token
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from django.views.generic import View
 import common
-from helpers import environs_helper, clusters_helper, autoscaling_groups_helper
+from helpers import environs_helper, clusters_helper, autoscaling_groups_helper, placements_helper
 from helpers import baseimages_helper
 from deploy_board.settings import IS_PINTEREST
 
 
-
 logger = logging.getLogger(__name__)
 
+
 class EnvCapacityConfigView(View):
+
     def get(self, request, name, stage):
         # cluster manager
         provider_list = None
@@ -40,6 +40,7 @@ class EnvCapacityConfigView(View):
         adv = False
         env = environs_helper.get_env_by_stage(request, name, stage)
         cluster_name = env.get('clusterName')
+        placements = None
         if IS_PINTEREST:
             provider_list = baseimages_helper.get_all_providers(request)
             basic_cluster_info = clusters_helper.get_cluster(request, cluster_name)
@@ -49,6 +50,11 @@ class EnvCapacityConfigView(View):
                 asg_cluster = autoscaling_groups_helper.get_group_info(request, cluster_name)
                 basic_cluster_info['asg_info'] = asg_cluster
                 basic_cluster_info['base_image_info'] = base_image
+                try:
+                    placements = placements_helper.get_simplified_by_ids(
+                        request, basic_cluster_info['placement'], basic_cluster_info['provider'], basic_cluster_info['cellName'])
+                except Exception as e:
+                    logger.warning('Failed to get placements: {}'.format(e))
 
             params = request.GET
             if params.get('adv'):
@@ -72,6 +78,7 @@ class EnvCapacityConfigView(View):
                 'basic_cluster_info': basic_cluster_info,
                 'adv': adv,
                 'create_new': create_new,
+                'placements': placements,
             }
             return HttpResponse(json.dumps(info), content_type="application/json")
 
@@ -92,7 +99,8 @@ class EnvCapacityConfigView(View):
             'provider_list': provider_list,
             'basic_cluster_info': basic_cluster_info,
             'adv': adv,
-            'create_new': create_new
+            'create_new': create_new,
+            'placements': placements,
         }
         data['info'] = json.dumps(data)
         return render(request, 'configs/capacity.html', data)
@@ -100,14 +108,12 @@ class EnvCapacityConfigView(View):
     def post(self, request, name, stage):
         logger.info("Post to capacity with data {0}".format(request.body))
         data = json.loads(request.body)
-        hosts =  data.get('hosts')
-        if hosts != None:
+        hosts = data.get('hosts')
+        if hosts:
             environs_helper.update_env_capacity(request, name, stage, capacity_type="HOST", data=hosts)
 
         groups = data.get("groups")
-        if groups != None:
-            environs_helper.update_env_capacity(request, name, stage, capacity_type="GROUP",
-                                                data=groups)
+        if groups:
+            environs_helper.update_env_capacity(request, name, stage, capacity_type="GROUP", data=groups)
 
         return self.get(request, name, stage)
-

deploy-board/deploy_board/static/js/components/capacitycomponents.js
@@ -188,23 +188,90 @@ Vue.component('in-rolling-alert', {
     props:['actionlink']
 });
 
+function getCapacityAlertMessage(isWarning, remainingCapacity, placements, increase) {
+    const errorMessage = `Insufficient combined remaining capacity in this cluster/auto scaling group. `;
+    const instruction = `You can attach additional placements to the corresponding clutter to increase` +
+                        ` total potential capacity at Cluster Configuration -> Advanced Settings.\n`;
+    const status = `Combined remaining capacity: ${remainingCapacity}\n` +
+                   `Current placement(s): ${JSON.stringify(placements, ['capacity', 'provider_name', 'abstract_name'], 2)}`;
+
+    if (isWarning) {
+        return errorMessage + `You can save this configuration but the cluster/ASG might run into capacity issues in the future. ` +
+                instruction + `Requested size increase: ${increase}\n` + status
+    } else {
+        // error
+        return errorMessage + `You shouldn't save this configuration because the cluster/ASG will run into capacity issues. ` +
+                instruction + `Requested size increase: ${increase}\n` + status;
+    }
+}
+
+function calculateImbalanceThreshold(totalIncrease, numPlacements) {
+    // average increase per placement
+    return (totalIncrease / numPlacements).toFixed()
+}
+
+function checkImbalance(placements, threshold) {
+    var insufficientPlacements = [];
+    var showImbalanceWarning = false;
+    for (var p of placements) {
+        if (p.capacity < threshold) {
+            showImbalanceWarning = true;
+            insufficientPlacements.push(p)
+        }
+    }
+    return showImbalanceWarning ? `The ASG might have AZ imbalance based on a potential increase of ${threshold} hosts per placement. `+
+        `Please pay attention to the following placements:\n` +
+        `${JSON.stringify(insufficientPlacements, ['capacity', 'provider_name', 'abstract_name'], 2)}` : '';
+}
 
 /**
  * The capacity button. This is shown when autoscaling is disabled or min\max size are the same.
  * In this case, only one capacity box is shown
  */
 Vue.component("static-capacity-config", {
-    template: '<div class="form-group">\
-    <label for="capacity" class="deployToolTip control-label col-xs-4" title="Number of hosts for this service">\
-        Capacity\
-    </label>\
-    <div class="col-xs-2" >\
-    <input class="form-control" v-bind:value="capacity" v-on:change="updateValue($event.target.value)" @keydown.enter.prevent=""></input>\
-    </div></div>',
-    props: ['capacity'],
+    template: `<div>
+    <div class="form-group">
+        <label for="capacity" class="deployToolTip control-label col-xs-4" title="Number of hosts for this service">
+            Capacity
+        </label>
+        <div class="col-xs-2" >
+            <input name="capacity" class="form-control" type="number" min="0" required
+                :value="capacity" @input="onCapacityChange($event.target.value)" @keydown.enter.prevent="">
+        </div>
+    </div>
+    <form-danger v-show="showSizeError" :alert-text="sizeError"></form-danger>
+    <form-warning v-show="showImbalanceWarning" :alert-text="imbalanceWarning"></form-warning>
+    </div>`,
+    props: {
+        originalCapacity: Number,
+        remainingCapacity: Number,
+        placements: Object,
+    },
+    data: function() {
+        return {
+            capacity: this.originalCapacity,
+            showSizeError: false,
+            showImbalanceWarning: false,
+            sizeError: '',
+            imbalanceWarning: '',
+        }
+    },
     methods: {
-        updateValue: function (value) {
-            this.$emit("change", value)
+        onCapacityChange: function (value) {
+            this.capacity = Number(value);
+            this.validateSize();
+            this.$emit('change', this.capacity );
+        },
+        validateSize: function () {
+            const sizeIncrease = this.capacity - this.originalCapacity;
+            if (sizeIncrease >= this.remainingCapacity) {
+                this.sizeError = getCapacityAlertMessage(false, this.remainingCapacity, this.placements, sizeIncrease);
+                this.showSizeError = true;
+            } else {
+                this.showSizeError = false;
+            }
+            this.imbalanceWarning = checkImbalance(this.placements, calculateImbalanceThreshold(sizeIncrease, this.placements.length));
+            this.showImbalanceWarning = this.imbalanceWarning != '';
         }
     }
 });
@@ -214,30 +281,106 @@ Vue.component("static-capacity-config", {
  * In this case, both min size and max size buttons are shown
  */
 Vue.component("asg-capacity-config", {
-    template: '<div class="form-group">\
-    <label for="capacity" class="deployToolTip control-label col-xs-4" title="Number of hosts for this service">\
-        Capacity\
-    </label>\
-            <div class="col-xs-2">\
-                <div class="input-group" >\
-                    <span class="input-group-addon">Min Size</span>\
-                    <input class="form-control" v-bind:value="minsize" v-on:change="updateMinSize($event.target.value)" @keydown.enter.prevent=""></input>\
-                </div>\
-            </div>\
-            <div class="col-xs-2">\
-                <div class="input-group" >\
-                    <span class="input-group-addon">Max Size</span>\
-                    <input class="form-control" v-bind:value="maxsize" v-on:change="updateMaxSize($event.target.value)" @keydown.enter.prevent=""></input>\
-                </div>\
-            </div>\
-    </div>',
-    props: ['minsize', 'maxsize'],
-    methods: {
-        updateMinSize: function (value) {
-            this.$emit('minchange', value)
+    template: `<div>
+    <div class="form-group">
+        <label for="capacity" data-toggle="tooltip" class="deployToolTip control-label" :class="labelBootstrapClass" :title="labelText">
+            {{ labelTitle }}
+        </label>
+        <div :class="inputBootstrapClass">
+            <div class="input-group">
+                <span class="input-group-addon">Min Size</span>
+                <input name="minSize" class="form-control" type="number" min="0" required
+                    :value="minSize" @input="onMinSizeChange($event.target.value)" @keydown.enter.prevent="" >
+            </div>
+        </div>
+        <div :class="inputBootstrapClass">
+            <div class="input-group">
+                <span class="input-group-addon">Max Size</span>
+                <input name="maxSize" class="form-control" type="number" min="0" required
+                    :value="maxSize" @input="onMaxSizeChange($event.target.value)" @keydown.enter.prevent="">
+            </div>
+        </div>
+    </div>
+    <form-danger v-show="showSizeError" :alert-text="sizeError"></form-danger>
+    <form-warning v-show="showSizeWarning" :alert-text="sizeWarning"></form-warning>
+    <form-warning v-show="showImbalanceWarning" :alert-text="imbalanceWarning"></form-warning>
+    </div>`,
+    props: {
+        labelBootstrapClass: {
+            type: String,
+            default: 'col-xs-4',
         },
-        updateMaxSize: function (value) {
-            this.$emit('maxchange', value)
+        inputBootstrapClass: {
+            type: String,
+            default: 'col-xs-2',
+        },
+        labelText: {
+            type: String,
+            default: 'Number of hosts for this service',
+        },
+        labelTitle: {
+            type: String,
+            default: 'Capacity'
+        },
+        currentSize: Number,
+        originalMinSize: Number,
+        originalMaxSize: Number,
+        remainingCapacity: Number,
+        placements: Object,
+    },
+    data: function() {
+        return {
+            minSize: this.originalMinSize,
+            maxSize: this.originalMaxSize,
+            showSizeError: false,
+            showSizeWarning: false,
+            showImbalanceWarning: false,
+            sizeError: '',
+            sizeWarning: '',
+            imbalanceWarning: '',
         }
+    },
+    methods: {
+        onMinSizeChange: function (value) {
+            this.minSize = Number(value);
+            if (this.maxSize < this.minSize) {
+                this.maxSize = this.minSize;
+            }
+            this.validateSize();
+            this.$emit('min-change', this.minSize);
+        },
+        onMaxSizeChange: function (value) {
+            this.maxSize = Number(value);
+            if (this.maxSize < this.minSize) {
+                this.minSize = this.maxSize;
+            }
+            this.validateSize();
+            this.$emit('max-change', this.maxSize);
+        },
+        validateSize: function() {
+            const minIncrease = this.minSize - this.originalMinSize;
+            const maxIncrease = this.maxSize - this.originalMaxSize;
+
+            if (minIncrease >= this.remainingCapacity) {
+                this.sizeError = getCapacityAlertMessage(false, this.remainingCapacity, this.placements, minIncrease);
+                this.showSizeError = true;
+            } else {
+                this.showSizeError = false;
+            }
+
+            if (!this.showSizeError && maxIncrease >= this.remainingCapacity) {
+                this.sizeWarning = getCapacityAlertMessage(true, this.remainingCapacity, this.placements, maxIncrease);
+                this.showSizeWarning = true;
+            } else {
+                this.showSizeWarning = false;
+            }
+
+            const avgSizeIncreasePerPlacement = calculateImbalanceThreshold(this.maxSize - this.currentSize, this.placements.length);
+            this.imbalanceWarning = checkImbalance(this.placements, avgSizeIncreasePerPlacement);
+            this.showImbalanceWarning = this.imbalanceWarning != '';
+        },
+    },
+    attached: function() {
+        this.validateSize();
     }
 });

deploy-board/deploy_board/static/js/components/sharedcomponents.js
@@ -264,14 +264,27 @@ Vue.component("help-table", {
 })
 
 /**
- * A form inline warning
+ * A form inline warning alert
  */
  Vue.component("form-warning", {
     template:'<div class="form-group"><div class="col-xs-2"></div>\
     <div class="col-xs-10">\
-        <div class="alert alert-warning">\
+        <div class="alert alert-warning" style="white-space: pre-wrap;">\
             <button v-if="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
-            <strong>Warning!</strong> {{ warningtext }}</div>\
+            <strong>Warning!</strong> {{ alertText }}</div>\
     </div></div>',
-    props: ['dismissible', 'warningtext']
+    props: ['dismissible', 'alertText']
+  })
+
+/**
+ * A form inline error alert
+ */
+ Vue.component("form-danger", {
+    template:'<div class="form-group"><div class="col-xs-2"></div>\
+    <div class="col-xs-10">\
+        <div class="alert alert-danger" style="white-space: pre-wrap;">\
+            <button v-if="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
+            <strong>Danger!</strong> {{ alertText }}</div>\
+    </div></div>',
+    props: ['dismissible', 'alertText']
   })

deploy-board/deploy_board/templates/clusters/cluster_capacity.tmpl
@@ -6,8 +6,16 @@
                 <fieldset id="clusterConfigFieldSetId">
                     <label-info title="Cluster State" name="Cluster State" v-bind:text="clusterstate" v-bind:styleclass="clusterStateStyle"></label-info>
                     <label-info title="Cell" name="Cell" v-bind:text="cell"></label-info>
-                    <static-capacity-config v-if="!autoscalingEnabled" v-bind:capacity="desiredCapacity" v-on:change="updateStatic"></static-capacity-config>
-                    <asg-capacity-config v-if="autoscalingEnabled" v-bind:minsize="minsize" v-bind:maxsize="maxsize" v-on:minchange="updateMin" v-on:maxchange="updateMax"></asg-capacity-config>
+                    <static-capacity-config v-if="!autoscalingEnabled"
+                        :original-capacity="cluster.capacity" @change="desiredCapacity = $event"
+                        :remaining-capacity="remainingCapacity" :placements="placements">
+                    </static-capacity-config>
+                    <asg-capacity-config v-if="autoscalingEnabled" :current-size="cluster.capacity"
+                        :original-min-size="asg_launch_info.minSize" @min-change="minsize = $event"
+                        :original-max-size="asg_launch_info.maxSize" @max-change="maxsize = $event"
+                        :remaining-capacity="remainingCapacity" :placements="placements">
+                    </asg-capacity-config>
+                    <form-warning v-show="showSizeWarning" v-bind:alert-text="sizeWarning"></form-warning>
                 </fieldset>
             </form>
         </div>
@@ -55,24 +63,13 @@
                 minsize: asg_launch_info.minSize,
                 maxsize: asg_launch_info.maxSize,
                 autoscalingEnabled:autoscalingEnabled,
-                canSaveCapacity: cluster.state === "NORMAL"
+                canSaveCapacity: cluster.state === "NORMAL",
+                showSizeWarning: false,
+                sizeWarning: "",
+                remainingCapacity: info.placements != null ?  info.placements.reduce((s, e) => s + e.capacity, 0) : Infinity,
+                placements: info.placements,
             },
             methods:{
-                updateStatic: function(value){
-                   this.desiredCapacity = Number(value)
-                },
-                updateMin:function(value){
-                    this.minsize = Number(value)
-                    if (this.maxsize<this.minsize){
-                        this.maxsize = this.minsize
-                    }
-                },
-                updateMax: function(value){
-                    this.maxsize = Number(value)
-                    if (this.maxsize<this.minsize){
-                        this.minsize = this.maxsize
-                    }
-                },
                 clickDialog: function(value){
                     if (value){
                         this.save();
@@ -80,18 +77,9 @@
                 },
                 save:function(){
                     if (!this.autoscalingEnabled){
-                        if (this.desiredCapacity<0){
-                            globalNotificationBanner.error = "Capacity cannot be less than 0"
-                            return
-                        }
                         //Set min and max size directly to group. Right now, cluster itself has no autoscaling info
                         this.minsize = this.desiredCapacity
                         this.maxsize = this.desiredCapacity
-                    }
-
-                    if (this.minsize<0 || this.maxsize<0 || this.minsize > this.maxsize){
-                            globalNotificationBanner.error = "Min and Max size cannot be less than 0 and Max size cannot be less than Min size"
-                            return
                     }
 
                     $.ajax({
@@ -106,7 +94,6 @@
                         },
                         success: function (data) {
                             globalNotificationBanner.info = "Capacity updated successfully"
-                            window.location.href='/env/{{ env.envName }}/{{ env.stageName }}/config/capacity/'
                         },
                         error: function (data) {
                             var optionalInstruction = '';

deploy-board/deploy_board/templates/clusters/cluster_configuration.html
@@ -66,7 +66,7 @@
                     <securityzone-help v-show="showSecurityZoneHelp" v-bind:data="securityZoneHelpData"></securityzone-help>
                     <placements-select label="Placements" title="Placements" showsubnettype="true" v-bind:selectoptions="placements" v-bind:assignpublicip="assignPublicIP" v-bind:subnettype="subnetType"
                     showhelp="true" v-on:helpclick="placementsHelpClick" v-on:assignpublicipclick="selectpublicip" v-on:subnetfilterclick="changeSubnetType"></placements-select>
-                    <form-warning v-show="showSubnetReplacementAlert" v-bind:warningtext="subnetReplacementWarning"></form-warning>
+                    <form-warning v-show="showSubnetReplacementAlert" v-bind:alert-text="subnetReplacementWarning"></form-warning>
                     <placements-help v-show="showPlacementsHelp" v-bind:data="placementsHelpData"></placements-help>
                     <div v-if="inAdvanced">
                         <div class="form-group"></div>

deploy-board/deploy_board/templates/groups/asg_config.html
@@ -1,4 +1,5 @@
 {% extends 'environs/env_base.html' %}
+{% load static %}
 {% load utils %}
 
 {% block breadcrumb-items %}
@@ -38,6 +39,8 @@
     </div>
 
     {% if asg_vm_config.asgStatus != "UNKNOWN" %}
+    <script type="text/javascript" src="{% static "js/components/sharedcomponents.js"%}"></script>
+    <script type="text/javascript" src="{% static "js/components/capacitycomponents.js"%}"></script>
     <div id="asgConfigPid" class="panel panel-default">
     {% include "groups/asg_config.tmpl" %}
     </div>

deploy-board/deploy_board/templates/groups/asg_config.tmpl
@@ -22,64 +22,40 @@
     </div>
 </div>
 <div id="autoscalingConfigId" class="collapse in panel-body">
-<div class="container-fluid">
-    <form id="autoscalingGroupConfigFormId" class="form-horizontal" role="form">
-        <input type="hidden" name="asgStatus" value="{{ asg.status }}" />
-        <input type="hidden" name="groupName" value="{{ group_name }}" />
-        <fieldset id="envConfigFieldSetId">
-            <div class="form-group">
-                <label for="minSize" class="deployToolTip control-label col-xs-2"
-                    data-toggle="tooltip"
-                    title="minimum number of hosts in one autoscaling group">
-                    Min Size (Current size is {{ group_size }})
-                </label>
-                <div class="col-xs-4">
-                    <input class="form-control" name="minSize" required="true" id="minSizeInput"
-                           type="text" value="{{ asg.minSize }}"/>
-                </div>
+    <div class="container-fluid">
+        <form id="autoscalingGroupConfigFormId" class="form-horizontal" role="form">
+            <input type="hidden" name="asgStatus" value="{{ asg.status }}">
+            <input type="hidden" name="groupName" value="{{ group_name }}">
+            <fieldset id="envConfigFieldSetId">
+                <asg-capacity-config
+                    original-min-size="{{ asg.minSize }}" original-max-size="{{ asg.maxSize }}" current-size="{{ group_size }}"
+                    label-bootstrap-class="col-xs-2" input-bootstrap-class="col-xs-4"
+                    label-title="Capacity (Current size is {{ group_size }})"
+                    label-text="Minimun and maximum number of hosts in one autoscaling group"
+                    :remaining-capacity="remainingCapacity" :placements="placements">
+                </asg-capacity-config>
 
-                <label for="maxSize" class="deployToolTip control-label col-xs-2"
-                    data-toggle="tooltip"
-                    title="maximum number of hosts in one autoscaling group">
-                    Max Size
-                </label>
-                <div class="col-xs-4">
-                    <input class="form-control" name="maxSize" required="true" id="maxSizeInput"
-                           type="text" value="{{ asg.maxSize }}"/>
-                </div>
-            </div>
-
-            <div id="minSizeWarningDivId" class="hidden">
                 <div class="form-group">
-                    <label class="control-label col-xs-2"></label>
-                    <div class="col-xs-10">
-                    <h4><span class="label label-danger">Minimum size is too low for your group!</span></h4>
+                    <label for="terminationPolicy" class="deployToolTip control-label col-xs-2"
+                            data-toggle="tooltip" title="termination policy">
+                        Termination Policy
+                    </label>
+                    <div class="col-xs-4">
+                        <select class="form-control" name="terminationPolicy" required="true" id="terminationPolicyInput">
+                        {% for policy in terminationPolicies %}
+                            {% if policy == asg.terminationPolicy %}
+                                <option value="{{policy}}" selected>{{policy}}</option>
+                            {% else %}
+                                <option value="{{policy}}">{{policy}}</option>
+                            {% endif %}
+                        {% endfor %}
+                        </select>
                     </div>
                 </div>
-            </div>
-
-            <div class="form-group">
-                <label for="terminationPolicy" class="deployToolTip control-label col-xs-2"
-                  data-toggle="tooltip"
-                  title="termination policy">
-                  Termination Policy
-                </label>
-                <div class="col-xs-4">
-                    <select class="form-control" name="terminationPolicy" required="true" id="terminationPolicyInput">
-                    {% for policy in terminationPolicies %}
-                        {% if policy == asg.terminationPolicy %}
-                            <option value="{{policy}}" selected>{{policy}}</option>
-                        {% else %}
-                            <option value="{{policy}}">{{policy}}</option>
-                        {% endif %}
-                    {% endfor %}
-                    </select>
-                </div>
-            </div>
-        </fieldset>
-      {% csrf_token %}
-       </form>
-</div>
+            </fieldset>
+        {% csrf_token %}
+        </form>
+    </div>
 </div>
 
 
@@ -103,6 +79,19 @@ $(function() {
     $.get('/groups/{{ group_name }}/autoscaling/get_pas_config/', function (response) {
         $('#pasConfigPid').html(response);
     });
+});
+
+var asgConfigVue = new Vue({
+    el: '#autoscalingConfigId',
+    data: {
+        {% if placements %}
+        placements: {{ placements | safe }},
+        remainingCapacity: {{placements| safe }}.reduce((s, e) => s + e.capacity, 0),
+        {% else %}
+        placements: {},
+        remainingCapacity: Infinity,
+        {% endif %}
+    }
 });
 
 function loadAsgScheduledActions() {

deploy-board/deploy_board/webapp/group_view.py
@@ -24,10 +24,8 @@ import json
 import logging
 import traceback
 
-from helpers import environs_helper, clusters_helper
-from helpers import groups_helper, baseimages_helper
-from helpers import specs_helper, autoscaling_groups_helper
-from helpers import autoscaling_metrics_helper
+from helpers import (environs_helper, clusters_helper, groups_helper, baseimages_helper,
+                     specs_helper, autoscaling_groups_helper, autoscaling_metrics_helper, placements_helper)
 from diff_match_patch import diff_match_patch
 from deploy_board import settings
 from helpers.exceptions import TeletraanException
@@ -307,7 +305,15 @@ def get_asg_config(request, group_name):
     policies = autoscaling_groups_helper.TerminationPolicy
     if asg_summary.get("sensitivityRatio", None):
         asg_summary["sensitivityRatio"] *= 100
-    scheduled_actions = autoscaling_groups_helper.get_scheduled_actions(request, group_name)
+
+    placements = None
+    try:
+        basic_cluster_info = clusters_helper.get_cluster(request, group_name)
+        if basic_cluster_info:
+            placements = placements_helper.get_simplified_by_ids(
+                request, basic_cluster_info['placement'], basic_cluster_info['provider'], basic_cluster_info['cellName'])
+    except Exception as e:
+        log.warning('Failed to get placements: {}'.format(e))
     content = render_to_string("groups/asg_config.tmpl", {
         "group_name": group_name,
         "asg": asg_summary,
@@ -316,6 +322,7 @@ def get_asg_config(request, group_name):
         "instanceType": launch_config.get("instanceType"),
         "csrf_token": get_token(request),
         "pas_config": pas_config,
+        "placements": json.dumps(placements),
     })
     return HttpResponse(json.dumps(content), content_type="application/json")
 

commit 3b12b355a0087e8a3d002c1eadee1be16a878b08

Use `is not None` so empty list can pass if check. (#1026)
Use is not None so empty list can pass if check. This is required when use tries to delete all hosts or groups.

Regression introduced in #987

Tests:
Test in local setup that the last existing capacity can be deleted.

deploy-board/deploy_board/webapp/capacity_views.py
@@ -109,11 +109,11 @@ class EnvCapacityConfigView(View):
         logger.info("Post to capacity with data {0}".format(request.body))
         data = json.loads(request.body)
         hosts = data.get('hosts')
-        if hosts:
+        if hosts is not None:
             environs_helper.update_env_capacity(request, name, stage, capacity_type="HOST", data=hosts)
 
         groups = data.get("groups")
-        if groups:
+        if groups is not None:
             environs_helper.update_env_capacity(request, name, stage, capacity_type="GROUP", data=groups)
 
         return self.get(request, name, stage)