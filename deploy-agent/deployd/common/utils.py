5d1453868b59357db57a54853644cbbe7e16d1e5
anhn <anhn@pinterest.com>
2022-08-26 22:04:09 +0000

    Add metrics for telefig is unavailable

--- deploy-agent/deployd/common/utils.py
+++ deploy-agent/deployd/common/utils.py
@@ -29,6 +29,7 @@ from deployd.common.stats import TimeElapsed, create_sc_increment, create_sc_tim
 
 log = logging.getLogger(__name__)
 
+TELEFIG_BINARY = "configure-serviceset"
 
 # noinspection PyProtectedMember
 def exit_abruptly(status=0):
@@ -147,3 +148,6 @@ def check_not_none(arg, msg=None):
         raise ValueError(msg)
     return arg
 
+def check_telefig_unavaialble_error(msg):
+    telefig_unavaialble_error = "{}: No such file or directory".format(TELEFIG_BINARY)
+    return msg.find(telefig_unavaialble_error) != -1
