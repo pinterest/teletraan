192635e285ce081d6a7e91a17e896c209234bd7e
Yongwen Xu <yongwen@users.noreply.github.com>
2022-09-06 16:34:41 -0700

    bump deployd version
    from the changes in PR https://github.com/pinterest/teletraan/pull/1033

--- deploy-agent/deployd/__init__.py
+++ deploy-agent/deployd/__init__.py
@@ -20,4 +20,4 @@ METRIC_PORT_HEALTH = int(os.getenv('METRIC_PORT_HEALTH')) if os.getenv('METRIC_P
 METRIC_CACHE_PATH = os.getenv('METRIC_CACHE_PATH', None)
 TELEFIG_BINARY = os.getenv('TELEFIG_BINARY', "")
 
-__version__ = '1.2.40'
+__version__ = '1.2.41'
