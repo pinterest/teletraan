b8b04dca70c26ef6ea9d26f79dd0a8bb6599445e
rsinha_pins <rsinha@pinterest.com>
2022-10-04 22:51:41 +0000

    adding root volume type field

--- deploy-board/deploy_board/webapp/cluster_view.py
+++ deploy-board/deploy_board/webapp/cluster_view.py
@@ -723,6 +723,7 @@ def get_aws_config_name_list_by_image(image_name):
         config_map['ebs_size'] = 500
         config_map['ebs_mount'] = '/backup'
         config_map['ebs_volume_type'] = 'gp3'
+        config_map['root_volume_type'] = 'gp3'
         config_map['root_volume_size'] = 100
         if image_name == DEFAULT_CMP_IMAGE:
             config_map['pinfo_role'] = 'cmp_base'
