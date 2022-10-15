0fb27e75ba75babbd4b317b37f92c059bb296f88
Yaqin Li <yaqinli@pinterest.com>
2022-10-14 17:14:19 -0700

    fix typos

--- deploy-board/deploy_board/webapp/cluster_view.py
+++ deploy-board/deploy_board/webapp/cluster_view.py
@@ -311,8 +311,8 @@ def get_base_images(request):
         'base_images': base_images,
         'provider_list': provider_list,
         'cells_list': cells_list,
-        'arch_list': arches_list,
-        #'arch_list': ['X86', 'ARM'],
+        'arches_list': arches_list,
+        #'arches_list': ['X86', 'ARM'],
         'pageIndex': index,
         'pageSize': DEFAULT_PAGE_SIZE,
         'disablePrevious': index <= 1,
@@ -441,8 +441,8 @@ def get_host_types(request):
     arches_list = cells_helper.get_by_provider(request, DEFAULT_PROVIDER)
 
     return render(request, 'clusters/host_types.html', {
-        'arch_list': arches_list,
-        #'arch_list': ['X86', 'ARM'],
+        'arches_list': arches_list,
+        #'arches_list': ['X86', 'ARM'],
         'host_types': host_types,
         'provider_list': provider_list,
         'pageIndex': index,
