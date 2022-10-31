0be982b142d151ea9b524afb371f5ecdb759a6ad
rsinha_pins <rsinha@pinterest.com>
2022-10-31 23:42:44 +0000

    make same line

--- deploy-service/common/src/main/java/com/pinterest/deployservice/handler/CommonHandler.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/handler/CommonHandler.java
@@ -181,15 +181,13 @@ public class CommonHandler {
             // TODO this is Slack specific, screw hipchat for now
             if (deployBean.getSuc_date() != null && deployBean.getSuc_date() != 0L) {
                 // This is failure after previous success
-                return String.format("%s/%s: can not deploy to all the newly provisioned hosts. See details <%s>. 
-                    This build is currently marked as %s.",
+                return String.format("%s/%s: can not deploy to all the newly provisioned hosts. See details <%s>. This build is currently marked as %s.",
                     envBean.getEnv_name(),
                     envBean.getStage_name(),
                     webLink,
                     tagBean == null? 'NOT SET': tagBean.getTag_value());
             } else {
-                return String.format("%s/%s: %s %s/%s failed. See details <%s>
-                    This build is currently marked as %s.",
+                return String.format("%s/%s: %s %s/%s failed. See details <%s>. This build is currently marked as %s.",
                     envBean.getEnv_name(),
                     envBean.getStage_name(),
                     action,
