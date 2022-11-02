5dc1499b64ac231e1bc3f427a2600e0dae69ef32
rsinha_pins <rsinha@pinterest.com>
2022-11-02 22:21:13 +0000

    making changes

--- deploy-service/common/src/main/java/com/pinterest/deployservice/handler/CommonHandler.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/handler/CommonHandler.java
@@ -154,11 +154,14 @@ public class CommonHandler {
         String webLink = deployBoardUrlPrefix + String.format("/env/%s/%s/deploy/",
             envBean.getEnv_name(),
             envBean.getStage_name());
-        TagBean tagBean = tagDAO.getLatestByTargetId(buildId);
+        TagBean tagBean = tagDAO.getLatestByTargetId(deployBean.getBuild_id());
+
+        //List<TagBean> = tagDAO.getByTargetId(buildId)
 
         String action = getDeployAction(deployType);
         if (state == DeployState.SUCCEEDING) {
             // TODO this is Slack specific, screw hipchat for now
+
 
             if (tagBean != null && tagBean.getValue() == TagValue.BAD_BUILD) {
                 return String.format("WARNING: %s/%s: %s %s/%s completed successfully, but running on bad build. See details <%s>",
