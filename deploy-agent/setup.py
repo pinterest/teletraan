728eb725c0e3a9747df100d8a8ed8eec222b06a4
Yaqin Li <yaqinli@pinterest.com>
2023-08-16 15:48:35 -0700

    bump gevent version

--- deploy-agent/setup.py
+++ deploy-agent/setup.py
@@ -25,8 +25,7 @@ console_scripts = ['deploy-agent = deployd.agent:main',
 
 install_requires = [
     "requests==2.20.0",
-    "gevent>=1.0.2,<=1.2.2; python_version < '3'",
-    "gevent>=1.0.2,<=1.5.0; python_version >= '3'",
+    "gevent>=20.12.0",
     "lockfile==0.10.2",
     "boto>=2.39.0",
     "python-daemon==2.0.6",
