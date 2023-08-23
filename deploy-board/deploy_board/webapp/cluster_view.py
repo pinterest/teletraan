a8bd354c3fff01045a86b1d6a626dd34ea10c483
Rodshell Fleurinord <rfleurinord@packer-mlpuppet.ec2.pin220.com>
2023-08-23 20:33:16 +0000

    updating default value on front end

--- deploy-board/deploy_board/webapp/cluster_view.py
+++ deploy-board/deploy_board/webapp/cluster_view.py
@@ -569,6 +569,7 @@ def create_host_type(request):
     host_type_info['mem'] = float(params['mem']) * 1024
     host_type_info['core'] = int(params['core'])
     host_type_info['storage'] = params['storage']
+    host_type_info['blessed_status'] = hosttypes_helper.BlessedStatusValues.ONBOARDING
     hosttypes_helper.create_host_type(request, host_type_info)
     return redirect('/clouds/hosttypes/')
 
