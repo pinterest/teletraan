d2c5de843883ca16859c4a800e98ee6ceb666fb5
YaqinLi <liyaqin1@gmail.com>
2023-09-22 21:17:21 +0000

    keep the schema change in deploy.sql instead of in a new schema-update file

--- tools/mysql/schema-update-19.sql
+++
@@ -1,9 +0,0 @@
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- ALWAYS BACKUP YOUR DATA BEFORE EXECUTING THIS SCRIPT
--- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# This script upgrade DB schema from version 18 to version 19
-
-ALTER TABLE agents ADD COLUMN container_health_status VARCHAR(32) NOT NULL DEFAULT "N/A (Not Applicable)";
-
-UPDATE schema_versions SET version=19;
