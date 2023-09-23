efb0ae6865d22c1dea9b25ef08f8844cb6eac203
YaqinLi <liyaqin1@gmail.com>
2023-09-23 01:17:29 +0000

    add debug info

--- deploy-service/common/src/main/java/com/pinterest/deployservice/handler/GoalAnalyst.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/handler/GoalAnalyst.java
@@ -320,8 +320,10 @@ public class GoalAnalyst {
         updateBean.setStage_start_date(System.currentTimeMillis());
         updateBean.setDeploy_stage(report.getDeployStage());
         if (report.getExtraInfo() == null) {
+            LOG.debug("yaqin-debug-noContent");
             updateBean.setContainer_Health_Status("N/A (Not Applicable)");
         } else {
+            LOG.debug("yaqin-debug-hasContent");
             updateBean.setContainer_Health_Status(report.getExtraInfo().getOrDefault("serviceHealth", "N/A (Not Applicable)"));
         }
 
