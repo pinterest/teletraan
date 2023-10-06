3af994b5097c8ab6f3eb5374759388de98ce4fbd
Tyler Ouyang <touyang@pinterest.com>
2023-10-06 12:20:10 -0700

    formate

--- deploy-service/common/src/main/java/com/pinterest/deployservice/events/BuildEvent.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/events/BuildEvent.java
@@ -5,6 +5,13 @@ import com.pinterest.teletraan.universal.events.AppEvent;
 
 public class BuildEvent extends AppEvent {
     private final BuildBean buildBean;
+    private final String action;
+
+    public BuildEvent(Object source, BuildBean buildBean, String action) {
+        super(source);
+        this.buildBean = buildBean;
+        this.action = action;
+    }
 
     public BuildBean getBuildBean() {
         return buildBean;
@@ -12,13 +19,5 @@ public class BuildEvent extends AppEvent {
 
     public String getAction() {
         return action;
-    }
-
-    private final String action;
-
-    public BuildEvent(Object source, BuildBean buildBean, String action) {
-        super(source);
-        this.buildBean = buildBean;
-        this.action = action;
     }
 }
