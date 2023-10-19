d6c36bc53f70ee51a373e09931ac0e13fdee855b
knguyen100000010 <63071572+knguyen100000010@users.noreply.github.com>
2023-10-19 13:58:31 -0400

    Round down diff time (#1305)

--- deploy-board/deploy_board/webapp/templatetags/utils.py
+++ deploy-board/deploy_board/webapp/templatetags/utils.py
@@ -405,20 +405,20 @@ def smartDate(timestamp):
         if second_diff < 120:
             return "a minute ago"
         if second_diff < 3600:
-            return str(second_diff / 60) + " minutes ago"
+            return str(second_diff // 60) + " minutes ago"
         if second_diff < 7200:
             return "an hour ago"
         if second_diff < 86400:
-            return str(second_diff / 3600) + " hours ago"
+            return str(second_diff // 3600) + " hours ago"
     if day_diff == 1:
         return "Yesterday"
     if day_diff < 7:
         return str(day_diff) + " days ago"
     if day_diff < 31:
-        return str(day_diff / 7) + " weeks ago"
+        return str(day_diff // 7) + " weeks ago"
     if day_diff < 365:
-        return str(day_diff / 30) + " months ago"
-    return str(day_diff / 365) + " years ago"
+        return str(day_diff // 30) + " months ago"
+    return str(day_diff // 365) + " years ago"
 
 
 @register.filter("shortenDesc")
