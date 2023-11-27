16728432268fb7e4e15909dd63246106829b8e61
Omar <osoriano@pinterest.com>
2023-11-27 10:13:47 -0800

    Rename stage type env var (#1349)
    * (trivial) whitespace fix
    
    * Rename stage type env var
    
    The Teletraan stage type is now exposed as an environment variable to
    the host.
    
    Rename the env var to match the expected name.

--- deploy-agent/tests/unit/deploy/common/test_config.py
+++ deploy-agent/tests/unit/deploy/common/test_config.py
@@ -3,9 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#  
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#    
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,7 +55,7 @@ class TestConfigFunctions(tests.TestCase):
         self.assertEqual(os.environ['DEPLOY_ID'], '123')
         self.assertEqual(os.environ['ENV_NAME'], 'pinboard')
         self.assertEqual(os.environ['STAGE_NAME'], 'beta')
-        self.assertEqual(os.environ['COMPUTE_STAGE_TYPE'], 'DEFAULT')
+        self.assertEqual(os.environ['COMPUTE_ENV_TYPE'], 'DEFAULT')
         self.assertEqual(self.config.get_target(), '/tmp/pinboard')
 
 
