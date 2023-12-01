61c19f117408aa98a0d96520c93ecd612135cd55
knguyen100000010 <khoiohk78@yahoo.com>
2023-12-01 09:26:44 -0500

    revert settings

--- deploy-board/deploy_board/settings.py
+++ deploy-board/deploy_board/settings.py
@@ -97,7 +97,7 @@ GUINEA_PIG_ENVS = os.getenv("GUINEA_PIG_ENVS", "").split(",")
 KAFKA_LOGGING_ADD_ON_ENVS = os.getenv("KAFKA_LOGGING_ADD_ON_ENVS", "").split(",")
 
 LOG_DIR = os.getenv("LOG_DIR")
-LOG_LEVEL = 'DEBUG'
+LOG_LEVEL = os.getenv("LOG_LEVEL")
 
 # Change to your domain or hosts
 if LOG_LEVEL == 'DEBUG':
