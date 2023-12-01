2bbedf7cbe125f08e4c27de99a061e33ca19485c
Rodshell Fleurinord (he/him) <69278532+rfleur01@users.noreply.github.com>
2023-12-01 12:50:00 -0800

    Revert "Add account id validator (#1348)" (#1356)
    This reverts commit 42de3504dfc52b0586c4fb2ba76ec16ac97eb773.
    
    Co-authored-by: Rodshell Fleurinord <rfleurinord@packer-mlpuppet.ec2.pin220.com>

--- deploy-service/common/src/main/java/com/pinterest/deployservice/pingrequests/PingRequestValidator.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/pingrequests/PingRequestValidator.java
@@ -1,9 +1,7 @@
 package com.pinterest.deployservice.pingrequests;
 
-import java.util.Set;
-
 import com.pinterest.deployservice.bean.PingRequestBean;
 
 public abstract class PingRequestValidator {
-    public abstract void validate(PingRequestBean bean, Set<String> accountAllowList) throws Exception;
+    public abstract void validate(PingRequestBean bean) throws Exception;
 }
