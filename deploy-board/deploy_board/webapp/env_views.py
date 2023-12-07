524ce081b57ab1789d0df2c1bd46dff90cbadabc
Rodshell Fleurinord <rfleurinord@packer-mlpuppet.ec2.pin220.com>
2023-12-07 01:16:39 +0000

    updating pinch link for stagetypes

--- deploy-board/deploy_board/webapp/env_views.py
+++ deploy-board/deploy_board/webapp/env_views.py
@@ -22,9 +22,8 @@ from django.views.generic import View
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.contrib import messages
-from deploy_board.settings import IS_PINTEREST
 from deploy_board.settings import TELETRAAN_DISABLE_CREATE_ENV_PAGE, TELETRAAN_REDIRECT_CREATE_ENV_PAGE_URL, \
-    IS_DURING_CODE_FREEZE, TELETRAAN_CODE_FREEZE_URL, TELETRAAN_JIRA_SOURCE_URL, TELETRAAN_TRANSFER_OWNERSHIP_URL, TELETRAAN_RESOURCE_OWNERSHIP_WIKI_URL, HOST_TYPE_ROADMAP_LINK
+    IS_DURING_CODE_FREEZE, TELETRAAN_CODE_FREEZE_URL, TELETRAAN_JIRA_SOURCE_URL, TELETRAAN_TRANSFER_OWNERSHIP_URL, TELETRAAN_RESOURCE_OWNERSHIP_WIKI_URL, HOST_TYPE_ROADMAP_LINK, STAGE_TYPE_INFO_LINK
 from deploy_board.settings import DISPLAY_STOPPING_HOSTS
 from deploy_board.settings import KAFKA_LOGGING_ADD_ON_ENVS
 from django.conf import settings
@@ -361,7 +360,7 @@ class EnvLandingView(View):
 
         if env["stageType"] == "DEFAULT":
             stageTypeWikiInfo = {}
-            stageTypeWikiInfo['link'] = "//pinch/teletraan-stagetypes"
+            stageTypeWikiInfo['link'] = STAGE_TYPE_INFO_LINK
             stageTypeWikiInfo['text'] = "pinch/teletraan-stagetypes"
             messages.add_message(request, messages.ERROR, "Please update the Stage Type to a value other than DEFAULT. See more details at ", stageTypeWikiInfo)
 
@@ -913,6 +912,7 @@ class EnvNewDeployView(View):
             "current_build": current_build,
             "pageIndex": 1,
             "pageSize": common.DEFAULT_BUILD_SIZE,
+            "stage_type_info_link": STAGE_TYPE_INFO_LINK,
         })
 
     def post(self, request, name, stage):
