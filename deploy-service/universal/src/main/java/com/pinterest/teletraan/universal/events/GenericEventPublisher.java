27c561965f844c66e94c794625725bd5db69215e
Tyler Ouyang <touyang@pinterest.com>
2023-12-07 11:58:13 -0800

    log at a different place

--- deploy-service/universal/src/main/java/com/pinterest/teletraan/universal/events/GenericEventPublisher.java
+++ deploy-service/universal/src/main/java/com/pinterest/teletraan/universal/events/GenericEventPublisher.java
@@ -30,8 +30,6 @@ public class GenericEventPublisher implements ReactiveEventPublisher {
             EmitResult result = eventsSink.tryEmitNext((AppEvent) event);
             if (result.isFailure()) {
                 LOG.error("Failed to publish event, cause: " + result);
-            } else {
-                LOG.debug("Published event: " + event);
             }
         } catch (Exception ex) {
             LOG.error("Failed to publish event", ex);
