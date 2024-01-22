da82fe8a6c37e0db1e5e26ed9d04bcde26055db0
Ian Roquebert <72234714+gzpcho@users.noreply.github.com>
2024-01-22 02:21:48 +0000

    fix: Missing Tuple import

--- deploy-agent/deployd/types/deploy_goal.py
+++ deploy-agent/deployd/types/deploy_goal.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Tuple
 from deployd.types.build import Build
 from deployd.types.deploy_stage import DeployStage
 
