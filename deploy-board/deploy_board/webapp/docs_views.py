5f1060516b423fa07b24f154ea4ab7932cffbb0f
Tyler Ouyang <tylercowen@me.com>
2024-01-23 21:08:21 -0800

    replaced

--- deploy-board/deploy_board/webapp/docs_views.py
+++ deploy-board/deploy_board/webapp/docs_views.py
@@ -3,9 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#  
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#    
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,22 @@
 from django.views.generic import View
 from django.shortcuts import render
 
+import os
+
+
 class SwaggerUIView(View):
     def get(self, request):
-        response = render(request, 'swagger-ui/dist/index.html', {
-            "token": request.session.get('oauth_token'),
-        })
+        teletraanServiceUrl = os.environ.get("TELETRAAN_SERVICE_URL", "http://localhost:8011")
+        rodimusServiceUrl = os.environ.get("RODIMUS_SERVICE_URL", "http://localhost:8012")
+        envStage = os.environ.get("ENV_STAGE", "local")
+        response = render(
+            request,
+            "swagger-ui/dist/index.html",
+            {
+                "token": request.session.get("oauth_token"),
+                "envStage": envStage,
+                "teletraanServiceUrl": teletraanServiceUrl,
+                "rodimusServiceUrl": rodimusServiceUrl,
+            },
+        )
         return response
