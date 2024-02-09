2c97721a81d7019d217b2a0412543e48b959a723
knguyen100000010 <khoiohk78@yahoo.com>
2024-02-09 14:05:22 -0500

    refuse to create alarm without scaling policy

--- deploy-board/deploy_board/webapp/group_view.py
+++ deploy-board/deploy_board/webapp/group_view.py
@@ -735,6 +735,13 @@ def add_alarms(request, group_name):
             alarm_info["metricSource"] = params["awsMetrics"]
     alarm_info["groupName"] = group_name
 
+    if len(alarm_info["scalingPolicies"]) == 0:
+        # Technically, an alarm can be created without an action (e.g. scaling policy)
+        # However, in the current context, an alarm must have an associated scaling policy.
+        # In this case, there is no scaling policy, so refuse to create new alarm.
+        messages.add_message(request, messages.ERROR, 'Alarm could not be created because there was no scaling policy. Create scaling policy first.')
+        return redirect("/groups/{}/config/".format(group_name))
+
     autoscaling_groups_helper.add_alarm(request, group_name, [alarm_info])
 
     return redirect("/groups/{}/config/".format(group_name))
