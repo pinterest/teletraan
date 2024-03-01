b7bf0b98fb9a07575f1cb78904c051624600cd02
Tyler Ouyang <tylercowen@me.com>
2024-02-29 18:07:20 -0800

    Remove deprecated org.hibernate.validator.constraints.NotEmpty

--- deploy-service/common/src/main/java/com/pinterest/deployservice/bean/PingRequestBean.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/bean/PingRequestBean.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *    
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,12 +15,12 @@
  */
 package com.pinterest.deployservice.bean;
 
-import org.apache.commons.lang.builder.ReflectionToStringBuilder;
-import org.hibernate.validator.constraints.NotEmpty;
-
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 
 public class PingRequestBean {
     @NotEmpty
@@ -109,7 +109,7 @@ public class PingRequestBean {
     public void setStageType(EnvType stageType) {
         this.stageType = stageType;
     }
-    
+
     public Set<String> getGroups() {
         return groups;
     }
