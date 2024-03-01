b4e8be202e4f2a6d862f2ca1ad103df88a7e36b4
ntascii <anhn@pinterest.com>
2024-03-01 23:32:09 +0000

    Update downloader to read allow lists from configs

--- deploy-agent/tests/unit/deploy/download/test_http_download_helper.py
+++ deploy-agent/tests/unit/deploy/download/test_http_download_helper.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from deployd.download.http_download_helper import HTTPDownloadHelper
+from deployd.common.config import Config
 import unittest
 from unittest import mock
 import logging
@@ -24,16 +25,17 @@ logger.level = logging.DEBUG
 class TestHttpDownloadHelper(unittest.TestCase):
 
     def setUp(self):
-        self.downloader = HTTPDownloadHelper(url="https://deploy1.com")
+        self.config = Config()
+        self.downloader = HTTPDownloadHelper(url="https://deploy1.com", config=self.config)
 
-    @mock.patch('deployd.download.http_download_helper.Config.get_http_download_allow_list')
+    @mock.patch('deployd.common.config.Config.get_http_download_allow_list')
     def test_validate_url_with_allow_list(self, mock_get_http_download_allow_list):
         mock_get_http_download_allow_list.return_value = ['deploy1.com', 'deploy2.com']
         result = self.downloader.validate_source()
         self.assertTrue(result)
         mock_get_http_download_allow_list.assert_called_once()
 
-    @mock.patch('deployd.download.http_download_helper.Config.get_http_download_allow_list')
+    @mock.patch('deployd.common.config.Config.get_http_download_allow_list')
     def test_validate_url_with_non_https(self, mock_get_http_download_allow_list):
         downloader = HTTPDownloadHelper(url="http://deploy1.com")
         mock_get_http_download_allow_list.return_value = ['deploy1', 'deploy2']
@@ -42,9 +44,9 @@ class TestHttpDownloadHelper(unittest.TestCase):
         self.assertFalse(result)
         mock_get_http_download_allow_list.assert_not_called()
 
-    @mock.patch('deployd.download.http_download_helper.Config.get_http_download_allow_list')
+    @mock.patch('deployd.common.config.Config.get_http_download_allow_list')
     def test_validate_url_without_allow_list(self, mock_get_http_download_allow_list):
-        mock_get_http_download_allow_list.return_value = None
+        mock_get_http_download_allow_list.return_value = []
         result = self.downloader.validate_source()
 
         self.assertTrue(result)
