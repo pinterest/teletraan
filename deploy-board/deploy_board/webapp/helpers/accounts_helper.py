284e2837edc8c75b90ac1fa86a65e030e3bfe330
Rui Zhu <ruizhu@pinterest.com>
2024-03-11 23:18:57 -0400

    create account details page

--- deploy-board/deploy_board/webapp/helpers/accounts_helper.py
+++ deploy-board/deploy_board/webapp/helpers/accounts_helper.py
@@ -1,5 +1,6 @@
-from deploy_board.webapp.helpers.rodimus_client import RodimusClient
 import logging
+
+from deploy_board.webapp.helpers.rodimus_client import RodimusClient
 
 log = logging.getLogger(__name__)
 
@@ -15,11 +16,11 @@ def get_all_accounts(request):
         return None
 
 
-def get_by_cell_and_id(request, cell, account_id):
+def get_by_cell_and_id(request, cell, account_id, provider="AWS"):
     try:
-        return rodimus_client.get(f"/accounts/AWS/{cell}/{account_id}", request.teletraan_user_id.token)
+        return rodimus_client.get(f"/accounts/{provider}/{cell}/{account_id}", request.teletraan_user_id.token)
     except Exception as e:
         log.error(f"Can't get account by cell and account_id: "
-                  f"cell = {cell}, account_id = {account_id}, error = {e}")
+                  f"provider={provider}, cell = {cell}, account_id = {account_id}, error = {e}")
         # return None for backward-compatibility
         return None
