d5cf92ee89e495439af84a3408c8967034005997
Tyler Ouyang <tylerwowen@users.noreply.github.com>
2024-03-13 10:28:14 -0700

    Add audit log (#1505)
    Add audit log in the OAuth middleware.
    
    Modernize middleware implementations per Django doc.
    
    Validated in local setup, will try again in dev1.

--- deploy-board/deploy_board/webapp/security.py
+++ deploy-board/deploy_board/webapp/security.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .auth import OAuth
@@ -23,10 +23,12 @@ import logging
 from .common import UserIdentity
 
 logger = logging.getLogger(__name__)
+audit_logger = logging.getLogger("deploy_board.audit")
 
 
 class DelegatedOAuthMiddleware(object):
-    def __init__(self):
+    def __init__(self, get_response):
+        self.get_response = get_response
         if settings.OAUTH_ENABLED:
             self.is_oauth_enabled = True
 
@@ -45,27 +47,27 @@ class DelegatedOAuthMiddleware(object):
             logger.info("OAuth is not enabled!")
             self.is_oauth_enabled = False
 
-    def process_request(self, request):
+    def __call__(self, request):
         if request.path.startswith('/auth/'):
             logger.info("Bypass OAuth redirect request " + request.path)
-            return None
+            return self.get_response(request)
 
         if request.path.startswith('/health_check/'):
             logger.info("Bypass health_check request " + request.path)
-            return None
+            return self.get_response(request)
 
         if not self.is_oauth_enabled:
             anonymous = UserIdentity(name="anonymous")
             request.teletraan_user_id = anonymous
-            return None
+            return self.get_response(request)
 
         # extract employee oauth token, redirect to OAuth if missing or invalid
         if self.oauth.validate_token(session=request.session):
             username = request.session.get('teletraan_user')
             token = request.session.get('oauth_token')
-            userId = UserIdentity(name=username, token=token)
-            request.teletraan_user_id = userId
-            return None
+            request.teletraan_user_id = UserIdentity(name=username, token=token)
+            audit_logger.info("audit", extra={"request": request})
+            return self.get_response(request)
         else:
             # TODO call logout to remove session cleanly
             # self.logout(request)
@@ -80,20 +82,20 @@ class FixedOAuthMiddleware(object):
     Use test oauth credentials to talk to backend instead of getting them
     from the OAuth.
     """
-    def __init__(self):
+    def __init__(self, get_response):
+        self.get_response = get_response
         if not settings.OAUTH_ENABLED and settings.TELETRAAN_SERVICE_FIXED_OAUTH_TOKEN:
             self._token = settings.TELETRAAN_SERVICE_FIXED_OAUTH_TOKEN
             logger.info("Using fixed OAuth credentials")
         else:
             self._token = None
 
-    def process_request(self, request):
+    def __call__(self, request):
         if self._token:
             request.teletraan_user_id = UserIdentity(name="anonymous", token=self._token)
-        return None
-
-    def logout(self, request):
-        pass
+            audit_logger.info("audit", extra={"request": request})
+            return self.get_response(request)
+        return HttpResponse("Unauthorized", status=401)
 
 
 def login_authorized(request):
@@ -173,12 +175,13 @@ def logout(request):
 
 
 class PRRMiddleware:
-    def __init__(self):
-        pass
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def process_response(self, request, response):
+    def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
+        response = self.get_response(request)
         response['Pragma'] = "no-cache"
         response['Cache-Control'] = "no-cache, no-store, must-revalidate"
         response['Expires'] = "Thu, 01 Jan 1970 00:00:00 GMT;"
