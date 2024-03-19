838ebcaf34c37022846c49f813abb6b967a522d3
Tyler Ouyang <tylercowen@me.com>
2024-03-18 18:29:50 -0700

    format

--- deploy-service/universal/src/test/java/com/pinterest/teletraan/universal/events/GenericEventPublisherTest.java
+++ deploy-service/universal/src/test/java/com/pinterest/teletraan/universal/events/GenericEventPublisherTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2024 Pinterest, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pinterest.teletraan.universal.events;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -9,7 +24,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.concurrent.TimeUnit;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -36,10 +50,10 @@ public class GenericEventPublisherTest {
         mockChildTestEventListener = mockEventListener(ChildTestEvent.class);
         mockSiblingTestEventListener = mockEventListener(SiblingTestEvent.class);
         doAnswer(
-                invocation -> {
-                    TimeUnit.MILLISECONDS.sleep(2);
-                    return null;
-                })
+                        invocation -> {
+                            TimeUnit.MILLISECONDS.sleep(2);
+                            return null;
+                        })
                 .when(mockSlowListener)
                 .onEvent(any());
     }
@@ -83,7 +97,8 @@ public class GenericEventPublisherTest {
         sut.terminate();
 
         // All events are processed
-        verify(mockResourceChangedEventListener, times(numEvents)).onEvent(eq(resourceChangedEvent));
+        verify(mockResourceChangedEventListener, times(numEvents))
+                .onEvent(eq(resourceChangedEvent));
     }
 
     @Test
