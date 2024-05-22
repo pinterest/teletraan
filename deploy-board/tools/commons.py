797d0a03da2289249c703a0a34ae4bbe970bf296
Ian Roquebert <72234714+gzpcho@users.noreply.github.com>
2024-05-22 17:36:40 -0500

    Fix `create_env.py` script to use non-DEFAULT stage type (#1624)

--- deploy-board/tools/commons.py
+++ deploy-board/tools/commons.py
@@ -3,9 +3,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#  
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-#    
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,13 +37,13 @@ def gen_random_num(size=8, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def create_env(name, stage):
+def create_env(name: str, stage: str):
     request = {}
     request["envName"] = name
     request["stageName"] = stage
-    request["stageType"]= "DEFAULT"
+    request["stageType"]= "PRODUCTION"
     env = environs_helper.create_env(REQUEST, request)
-    print("Successfully created env %s." % env['id'])
+    print(f"Successfully created env {env['id']}")
     return env
 
 
