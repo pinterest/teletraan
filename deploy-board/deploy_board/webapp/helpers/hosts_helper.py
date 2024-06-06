e8397fdf80fca6faa80bccfe3b169daf480500b1
CJ <163035983+Cjpilbdev@users.noreply.github.com>
2024-06-06 11:39:30 -0500

    added host protection status  (#1640)
    * added is protected
    
    * fix style
    
    * fix style
    
    fix style
    
    fix style
    
    fix error url button protect host
    
    add label
    
    added label
    
    * added missing import
    
    * pr suggestions
    
    * moved button
    
    * added condition button
    
    * fix show in list
    
    * fix button
    
    * fix error
    
    * added alert

--- deploy-board/deploy_board/webapp/helpers/hosts_helper.py
+++ deploy-board/deploy_board/webapp/helpers/hosts_helper.py
@@ -13,19 +13,32 @@
 # limitations under the License.
 
 # -*- coding: utf-8 -*-
-"""Collection of all hosts related calls
-"""
+"""Collection of all hosts related calls"""
+
 from deploy_board.webapp.helpers.deployclient import DeployClient
 from deploy_board.webapp.helpers.cmdbapiclient import CmdbApiClient
+from deploy_board.webapp.helpers.rodimus_client import RodimusClient
 
 deployclient = DeployClient()
 cmdbapi_client = CmdbApiClient()
+rodimus_client = RodimusClient()
+
 
 def get_hosts_by_name(request, host_name):
     return deployclient.get("/hosts/%s" % host_name, request.teletraan_user_id.token)
 
+
 def query_cmdb(query, fields, account_id):
     return cmdbapi_client.query(query, fields, account_id)
 
+
 def get_cmdb_host_info(host_id, account_id):
     return cmdbapi_client.get_host_details(host_id, account_id)
+
+
+def get_hosts_is_protected(request, host_ids):
+    return rodimus_client.post(
+        "/hosts/state?actionType=PROTECTED",
+        request.teletraan_user_id.token,
+        data=host_ids,
+    )
