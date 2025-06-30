78d2a8a65b7c7c9775c55a968a0a0fd2cebc21df
Omar Soriano <osoriano2@gmail.com>
2025-06-30 18:16:36 +0000

    Handle network exception in tag worker
    The tag worker is used to sync host tags in the Teletraan database.
    Since this task runs in the background, eventually a network error can
    be hit.
    
    When the network error is hit, handle the exception so that the tag
    worker can retry on the next run. Otherwise, the deploy constraints can
    end up in an unrecoverable error state

--- deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/worker/DeployTagWorker.java
+++ deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/worker/DeployTagWorker.java
@@ -22,6 +22,7 @@ import com.pinterest.deployservice.db.DatabaseUtil;
 import com.pinterest.deployservice.rodimus.RodimusManager;
 import com.pinterest.teletraan.universal.metrics.ErrorBudgetCounterFactory;
 import io.micrometer.core.instrument.Counter;
+import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.*;
@@ -180,9 +181,9 @@ public class DeployTagWorker implements Runnable {
                     LOG.info("DB lock operation is successful: get lock {}", lockName);
                     try {
                         processEachEnvironConstraint(job);
-                    } catch (SQLException e) {
+                    } catch (ConnectException | SQLException e) {
                         LOG.error(
-                                "failed to process job due to SQLException: {} Error {} stack {}",
+                                "failed to process job due to handled exception: {} Error {} stack {}",
                                 job,
                                 ExceptionUtils.getRootCauseMessage(e),
                                 ExceptionUtils.getStackTrace(e));
