1e843986b4c74f977ce15db45a46090e54ef4712
Navid Fallah <nfallah@pinterest.com>
2025-09-11 17:22:06 +0000

    Add missing import

--- deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/resource/EnvInfras.java
+++ deploy-service/teletraanservice/src/main/java/com/pinterest/teletraan/resource/EnvInfras.java
@@ -24,6 +24,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import org.slf4j.Logger;
