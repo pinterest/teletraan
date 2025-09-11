a765f0f07a8490f4c665c93bb59fc1a03a52210e
Navid Fallah <nfallah@pinterest.com>
2025-09-11 16:42:35 +0000

    Fixed InfraBean

--- deploy-service/common/src/main/java/com/pinterest/deployservice/bean/InfraBean.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/bean/InfraBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016-2024 Pinterest, Inc.
+ * Copyright (c) 2016-2025 Pinterest, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ package com.pinterest.deployservice.bean;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
-public class InfraBean implements Updatable {
+public class InfraBean {
     private String accountId;
 
     public String getAccountId() {
