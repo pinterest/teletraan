264bb93002c1db1d54914ff2e4ae4cb95f65ff32
knguyen100000010 <khoiohk78@yahoo.com>
2025-10-24 10:12:09 -0400

    format

--- deploy-service/common/src/main/java/com/pinterest/deployservice/common/ErrorResponse.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/common/ErrorResponse.java
@@ -15,6 +15,9 @@
  */
 package com.pinterest.deployservice.common;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
     private final int code;
     private final String message;
@@ -22,13 +25,5 @@ public class ErrorResponse {
     public ErrorResponse(int code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
