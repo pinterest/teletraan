63c74c6fecfddf64fd100b43eb62da5d1789d8f5
knguyen100000010 <khoiohk78@yahoo.com>
2025-10-24 07:36:04 -0400

    proper response body

--- deploy-service/common/src/main/java/com/pinterest/deployservice/common/DeployInternalExceptionMapper.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/common/DeployInternalExceptionMapper.java
@@ -1,11 +1,21 @@
 package com.pinterest.deployservice.common;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class DeployInternalExceptionMapper extends BaseExceptionMapper<DeployInternalException> {
     public DeployInternalExceptionMapper() {
-        super(Response.Status.BAD_REQUEST, "text/plain");
+        super(Response.Status.BAD_REQUEST, MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public Response toResponse(DeployInternalException exception) {
+        ErrorResponse error = new ErrorResponse(Response.Status.BAD_REQUEST.getStatusCode(), exception.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
     }
 }
---
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/common/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.pinterest.deployservice.common;
+
+public class ErrorResponse {
+    private final int code;
+    private final String message;
+
+    public ErrorResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
