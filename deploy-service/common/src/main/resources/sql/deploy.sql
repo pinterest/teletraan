07c4c0a364fd235337b69e73197e79ae810606c8
Navid Fallah <nfallah@nfallah-7H6N3X7.dyn.pinadmin.com>
2025-10-30 14:55:33 -0700

    [CDP-10049] Implement a new Teletraan worker to apply infra changes

--- deploy-service/common/src/main/resources/sql/deploy.sql
+++ deploy-service/common/src/main/resources/sql/deploy.sql
@@ -313,7 +313,7 @@ CREATE TABLE IF NOT EXISTS infra_jobs (
     infra_config        VARCHAR(8192)   NOT NULL, -- serialized json, need to adjust size
     create_at           BIGINT          NOT NULL,
     last_update_at      BIGINT          NOT NULL,
-    status              CHAR(11)        NOT NULL DEFAULT "INITIALIZED" -- (INITIALIZED, PROCESSING, SUCCEEDED, FAILED)
+    status              CHAR(11)        NOT NULL DEFAULT "INITIALIZED", -- (INITIALIZED, PROCESSING, SUCCEEDED, FAILED)
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 CREATE INDEX infra_jobs_priority_idx ON infra_jobs(create_at, status);
