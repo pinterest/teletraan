a11c0bec0a19a80ea803f63c86172f8790d36326
Tyler Ouyang <tylerwowen@users.noreply.github.com>
2025-11-04 15:07:33 -0800

    Fix generic exception handling (#1881)
    Reverted c2935fccaed144be2a1ee539a0708005f554df33
    It has a known issue of mapping all `DeployInternalException` to 400. 
    - Most should be 400, we will need to address them. #1576 for instance
    - Some should be 500 
    - Some should be 409 Conflict
    
    
    Besides, after deploying to prod, we observe a new more issue
    1. Exception logs were significantly reduced due to the bypass of the `io.dropwizard.jersey.errors.LoggingExceptionMapperio.dropwizard.jersey.errors.LoggingExceptionMapper`. It can potentially make debugging harder. 
    <img width="1244" height="261" alt="Screenshot 2025-11-04 at 2 58 58 PM" src="https://github.com/user-attachments/assets/257e9034-db53-46a3-a410-7dbff59367cc" />
    
    This fix only address the specific issue and doesn't include side effects (hopefully).

--- deploy-service/common/src/main/java/com/pinterest/deployservice/common/InvalidBuildException.java
+++ deploy-service/common/src/main/java/com/pinterest/deployservice/common/InvalidBuildException.java
@@ -15,20 +15,11 @@
  */
 package com.pinterest.deployservice.common;
 
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-
-public abstract class BaseExceptionMapper<T extends Throwable> implements ExceptionMapper<T> {
-    private final Response.Status status;
-    private final String mediaType;
-
-    protected BaseExceptionMapper(Response.Status status, String mediaType) {
-        this.status = status;
-        this.mediaType = mediaType;
-    }
-
-    @Override
-    public Response toResponse(T exception) {
-        return Response.status(status).entity(exception.getMessage()).type(mediaType).build();
+public class InvalidBuildException extends Exception {
+    public InvalidBuildException(String buildName, String configuredBuildName) {
+        super(
+                String.format(
+                        "Build name (%s) does not match stage config (%s).",
+                        buildName, configuredBuildName));
     }
 }
